c07e1ec938bca35fdb2824cb4aac5b77d8da3824
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2022-04-25 21:21:42 +0300

    Update 739. Daily Temperatures.py

--- leetcode/739. Daily Temperatures.py
+++ leetcode/739. Daily Temperatures.py
@@ -7,14 +7,6 @@ class Solution:
 
         for index, value in enumerate(temperatures):
 
-            if len(stack) == 0:
-                stack.append((index, value))
-                continue
-
-            if value <= stack[-1][1]:
-                stack.append((index, value))
-                continue
-
             while stack and value > stack[-1][1]:
                 output[stack[-1][0]] = index - stack[-1][0]
                 stack.pop()
