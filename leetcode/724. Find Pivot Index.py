32eed83774f5a37e75502df749a3d62e91550b01
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2022-05-08 09:08:12 +0300

    find pivot index

---
+++ leetcode/724. Find Pivot Index.py
@@ -0,0 +1,13 @@
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        
+        prefix = [0] * (len(nums) + 1)
+        
+        for i in range(1, len(nums) + 1):
+            prefix[i] = prefix[i-1] + nums[i-1]
+        
+        for i in range(1, len(prefix)):
+            if prefix[i-1] == prefix[-1] - prefix[i]:
+                return i-1
+            
+        return - 1
