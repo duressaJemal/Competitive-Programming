b7959d686715301e4a5d2b740b62d6be33eac44b
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2022-06-22 14:46:55 +0300

    heap

---
+++ leetcode/1046. Last Stone Weight.py
@@ -0,0 +1,25 @@
+# https://leetcode.com/problems/last-stone-weight/submissions/
+
+import heapq
+
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        
+        if len(stones) == 1: return stones[0]
+        heap = []
+        
+        for stone in stones:
+            heapq.heappush(heap, -stone)
+        
+        while len(heap) > 1:
+            
+            y = -heapq.heappop(heap)
+            x = -heapq.heappop(heap)
+        
+            if y != x:
+                heapq.heappush(heap, x - y)
+            
+        if len(heap) == 0: return 0
+        return -heap[0]
+        
+        
