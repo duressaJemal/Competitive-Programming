c56500219be3148b785ae14031c1f9d789b5f1bf
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2022-07-05 11:22:24 +0300

    Update 402. Remove K Digits.py

--- leetcode/402. Remove K Digits.py
+++ leetcode/402. Remove K Digits.py
@@ -1,17 +1,19 @@
 # link https://leetcode.com/problems/remove-k-digits/
+
+# time: O(n)
+# space: O(n)
+
 class Solution:
     def removeKdigits(self, num: str, k: int) -> str:
-        
-        num = list(num)
+        nums = list(num)
         stack = []
-        
-        for n in num:
+        for n in nums:
             while stack and n < stack[-1] and k > 0:
                 stack.pop()
                 k -= 1
             stack.append(n)
-        
-        # slices the list if k > 0
-        stack = stack[:len(stack)-k]
-        
+        stack = stack[:len(stack) - k]
         return "".join(stack).lstrip("0") or "0"
+            
+                
+            
