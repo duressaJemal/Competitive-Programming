bcd15ea50d1ad28729a92ff5548838a928882095
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2022-07-08 14:15:02 +0300

    S

--- leetcode/876. Middle of the Linked List.py
+++ leetcode/876. Middle of the Linked List.py
@@ -1,23 +1,14 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+# link: https://leetcode.com/problems/middle-of-the-linked-list/submissions/
+
+# time: O(n)
+# space: O(1)
+
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
         
-        fast, slow = head, head
         
-        while fast:
-            
-            if fast.next == None:
-                return slow
-            
-            elif fast.next and fast.next.next == None:
-                return slow.next
-            
-            else:
-                
-                fast = fast.next.next
-                slow = slow.next
-            
+        slow, fast, = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
