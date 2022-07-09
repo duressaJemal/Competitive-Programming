1590f521c48d520a234f741d838145b494db4831
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2022-07-09 10:37:02 +0300

    S

--- leetcode/560. Subarray Sum Equals K.py
+++ leetcode/560. Subarray Sum Equals K.py
@@ -1,20 +1,27 @@
+# link: https://leetcode.com/problems/subarray-sum-equals-k/submissions/
+
+# time: O(n)
+# space: O(n)
+
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         
-        summation = 0
-        prefix = {0:1}
+        store = {0: 1}
+        total = 0
         output = 0
-        
-        for i in range(len(nums)):
-            
-            summation += nums[i]
-            
-            if summation - k in prefix:
-                output += prefix[summation - k]
+        n = len(nums)
+        for i in range(n):
+            total += nums[i]
+            if total - k in store:
+                output += store[total - k]
                 
-            if summation in prefix:
-                prefix[summation] += 1
+            if total in store:
+                store[total] += 1
             else:
-                prefix[summation] = 1
-            
+                store[total] = 1 
         return output
+                
+            
+            
+            
+                
