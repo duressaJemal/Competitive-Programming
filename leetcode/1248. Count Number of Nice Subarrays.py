f25ccdddb80d6676ce8b7aef7cf91b784f09b1b4
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2022-07-09 10:59:37 +0300

    S

--- leetcode/1248. Count Number of Nice Subarrays.py
+++ leetcode/1248. Count Number of Nice Subarrays.py
@@ -1,23 +1,25 @@
-#link https://leetcode.com/problems/subarray-sum-equals-k/
+# link: https://leetcode.com/problems/count-number-of-nice-subarrays/submissions/
+
+
+# time: O(n)
+# space: O(n)
 
 class Solution:
     def numberOfSubarrays(self, nums: List[int], k: int) -> int:
         
-        odd_prefix = 0
+        store = {0: 1}
+        total = 0
         output = 0
-        summation = {0:1}
-        
         for i in range(len(nums)):
-            
             if nums[i] % 2 != 0:
-                odd_prefix += 1
-       
-            if odd_prefix - k in summation:
-                output += summation[odd_prefix - k]
-            
-            if odd_prefix in summation:
-                summation[odd_prefix] += 1
+                total += 1
+            if total - k in store:
+                output += store[total - k]
+            if total in store:
+                store[total] += 1
             else:
-                summation[odd_prefix] = 1
-            
+                store[total] = 1
         return output
+                
+        
+        
