9ad058bef9e5af465e33ebf778d08a162b909b41
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2022-07-10 23:26:20 +0300

    s

--- leetcode/904. Fruit Into Baskets.py
+++ leetcode/904. Fruit Into Baskets.py
@@ -1,34 +1,33 @@
+# link: https://leetcode.com/problems/fruit-into-baskets/submissions/
+
+# time: O(n)
+# space: O(n)
+
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
         
-        container = []
-        counter = {}
+        start, end, longest = 0, 0, 0
+        n = len(fruits)
+        fruit_types = set()
+        container = {}
         
-        start = 0
-        end = 0
-        longest = 0
-        
-        while end < len(fruits):
+        while end < n:
             
-            # if our container is full and the next number not in container
-            if len(container) == 2 and fruits[end] not in container: 
-                
-                counter[fruits[start]] -= 1 # keeps track of counter values
-                if counter[fruits[start]] == 0: # removal condition from container
-                    container.remove(fruits[start])
+            fruit_types.add(fruits[end])
+            container[fruits[end]] = 1 + container.get(fruits[end], 0)
+            
+            while len(fruit_types) > 2:
+                container[fruits[start]] -= 1
+                if container[fruits[start]] == 0:
+                    fruit_types.remove(fruits[start])
                 start += 1
-                
-            else:
-                #expand
-                if fruits[end] not in container: # avoids adding duplicate values to container
-                    container.append(fruits[end])
-                    
-                if fruits[end] in counter: # keep track of our fruit types
-                    counter[fruits[end]] += 1
-                else:
-                    counter[fruits[end]] = 1
-                
-                longest = max(longest, end-start+1) # our longest window size
-                end += 1
+            
+            longest = max(longest, end - start + 1)
+            end += 1
         
         return longest
+                    
+                
+            
+            
+            
