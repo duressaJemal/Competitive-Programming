a3f1681590de32cdb0eb9db9d0d9987bd06583a6
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2022-07-10 12:40:23 +0300

    N

---
+++ leetcode/2086. Minimum Number of Buckets Required to Collect Rainwater from Houses.py
@@ -0,0 +1,21 @@
+# link: https://leetcode.com/problems/minimum-number-of-buckets-required-to-collect-rainwater-from-houses/
+
+class Solution:
+    def minimumBuckets(self, street: str) -> int:
+        street = list(street)
+        print(street)
+        num = 0
+        for i in range(len(street)):
+            if street[i] == "H":
+                if i > 0 and street[i-1] == "B":
+                    continue
+                if i < len(street) - 1 and street[i+1] == ".":
+                    street[i+1] = "B"
+                    num += 1
+                    continue
+                if i > 0 and street[i-1] == ".":
+                    street[i-1] = "B"
+                    num += 1
+                    continue
+                return -1
+        return num
