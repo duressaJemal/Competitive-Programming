6cac4b7f854d3b56586991f6fbac0ee6ae5a9d7f
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2022-07-10 12:30:06 +0300

    N

---
+++ leetcode/1642. Furthest Building You Can Reach.py
@@ -0,0 +1,14 @@
+# link: https://leetcode.com/problems/furthest-building-you-can-reach/submissions/
+
+class Solution:
+    def furthestBuilding(self, A, bricks, ladders):
+        heap = []
+        for i in range(len(A) - 1):
+            d = A[i + 1] - A[i]
+            if d > 0:
+                heapq.heappush(heap, d)
+            if len(heap) > ladders:
+                bricks -= heapq.heappop(heap)
+            if bricks < 0:
+                return i
+        return len(A) - 1
