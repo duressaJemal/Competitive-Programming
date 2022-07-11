5d93e868bf07083bf3f95f18f25e9a40a16398a0
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2022-07-11 10:33:04 +0300

    s

--- leetcode/347. Top K Frequent Elements.py
+++ leetcode/347. Top K Frequent Elements.py
@@ -1,9 +1,6 @@
 # https://leetcode.com/problems/top-k-frequent-elements/submissions/
 
-import heapq
-
 # heap
-
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         
@@ -11,20 +8,19 @@ class Solution:
         counter = collections.Counter(nums)
         
         heap = [(-value, key) for key, value in counter.items()]
-        heapq.heapify(heap)
+        heapify(heap)
         
         while k:
             
-            output.append(heapq.heappop(heap)[1])
+            output.append(heappop(heap)[1])
             k -= 1
         
         return output
             
 # sorting
-
 class Solution:
     def topKFrequent(self, nums, k):
-
+        
         d = {}
         output = []
         for i in nums:
@@ -33,13 +29,11 @@ class Solution:
                 d[i] += 1
             else:
                 d[i] = 1
-
+    
         freqent = sorted(d.items(), key=lambda kv: (kv[1]), reverse=True)
         for i in range(k):
             output.append(freqent[i][0])
 
         return output
             
-        
-        
         
