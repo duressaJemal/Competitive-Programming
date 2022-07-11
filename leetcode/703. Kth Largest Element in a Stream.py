09c9c3433fee83f271dff50471dfcc53d8c89769
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2022-07-11 10:27:41 +0300

    S

--- leetcode/703. Kth Largest Element in a Stream.py
+++ leetcode/703. Kth Largest Element in a Stream.py
@@ -1,23 +1,16 @@
 # https://leetcode.com/problems/kth-largest-element-in-a-stream/
 
-import heapq
-
 class KthLargest:
 
     def __init__(self, k: int, nums: List[int]):
         self.heap = nums
-        heapq.heapify(self.heap)
-        print(self.heap)
+        heapify(self.heap)
         self.k = k
-        
-        while len(self.heap) > self.k:
-            heapq.heappop(self.heap)
-            
+
     def add(self, val: int) -> int:
         
-        heapq.heappush(self.heap, val)
+        heappush(self.heap, val)
         while len(self.heap) > self.k:
-            heapq.heappop(self.heap)
+            heappop(self.heap)
             
         return self.heap[0]
-
