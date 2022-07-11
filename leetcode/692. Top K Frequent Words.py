76e2dfc029f3b242926f36b68ddbe8c08de75dbb
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2022-07-11 10:58:35 +0300

    s

--- leetcode/692. Top K Frequent Words.py
+++ leetcode/692. Top K Frequent Words.py
@@ -1,5 +1,8 @@
 # https://leetcode.com/problems/top-k-frequent-words/submissions/
 
+# time: O(n * log(k)) 
+# space: O(n)
+
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
         
@@ -7,11 +10,13 @@ class Solution:
         output = []
         
         heap = [(-value, key) for key, value in counter.items()]
-        heapq.heapify(heap)
+        heapify(heap)
         
         while k:
-            output.append(heapq.heappop(heap)[1])
+            output.append(heappop(heap)[1])
             k -= 1
         
         return output
         
+        
+# follow-up: why is it n not k
