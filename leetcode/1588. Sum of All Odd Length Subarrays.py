8c435111e5dfa02f308ccc5a0e0e959597a2190a
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2022-07-17 22:56:22 +0300

    n

---
+++ leetcode/1588. Sum of All Odd Length Subarrays.py
@@ -0,0 +1,29 @@
+# link: https://leetcode.com/problems/sum-of-all-odd-length-subarrays/
+
+# Naive
+
+# time: O(n^2)
+# space: O(n)
+
+class Solution:
+    def sumOddLengthSubarrays(self, arr: List[int]) -> int:
+        
+        n = len(arr)
+        SUM = 0
+        
+        for length in range(1, n + 1, 2):
+            
+            end = length
+            start = 0
+
+            while end <= n:
+                SUM += sum(arr[start: end])
+                start += 1
+                end  += 1
+            
+        return SUM
+          
+                
+                
+            
+        
