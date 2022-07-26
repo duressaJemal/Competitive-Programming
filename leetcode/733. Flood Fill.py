8c0220f19ea054349df6573c58cc4600d973ee6e
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2022-07-26 14:31:40 +0300

    S

--- leetcode/733. Flood Fill.py
+++ leetcode/733. Flood Fill.py
@@ -1,23 +1,72 @@
+# link: https://leetcode.com/problems/flood-fill/submissions/
+
+# DFS
+
+# time: O(n*m)
+# space: O(n*m)
+
 class Solution:
-    def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
         
-        n = len(image)
-        m = len(image[0])
-        original_color = image[sr][sc]
+        m = len(image)
+        n = len(image[0])
+        old_color = image[sr][sc]
         visited = set()
         
-        def dfs(r, c):
+        def dfs(image, r, c):
             
-            image[r][c] = newColor
-            visited.add((r,c))
+            if r < 0 or r >= m or c < 0 or c >= n: return 
+            if image[r][c] != old_color: return
+            if (r, c) in visited: return
             
-            for direction in [(-1, 0), (0, -1), (0, 1), (1, 0)]:
-                new_row  = r + direction[0]
-                new_col = c + direction[1]
-                
-                if (new_row, new_col) not in visited and 0 <= new_row < n and 0 <= new_col < m and image[new_row][new_col] == original_color:
-                    dfs(new_row, new_col)
-                
-        dfs(sr, sc)
+            visited.add((r, c))
+            image[r][c] = color
+            
+            dfs(image, r - 1, c)
+            dfs(image, r + 1, c)
+            dfs(image, r, c - 1)
+            dfs(image, r, c + 1)
+            
         
+        dfs(image, sr, sc)
         return image
+            
+        
+        
+# BFS
+
+# time: O(n*m)
+# space: O(n*m)
+
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        
+        m = len(image)
+        n = len(image[0])
+        old_color = image[sr][sc]
+        queue = deque([(sr, sc)])
+        
+        direction = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        
+        if image[sr][sc] == color: return image
+        
+        while queue:
+            
+            r, c = queue.popleft()
+            image[r][c] = color
+            
+            for dirc in direction:
+                
+                nr = r + dirc[0]
+                nc = c + dirc[1]
+                
+                if nr < 0 or nr >= m or nc < 0 or nc >= n: continue
+                if image[nr][nc] != old_color: continue
+                    
+                queue.append((nr, nc))
+            
+        return image
+        
+            
+            
+            
