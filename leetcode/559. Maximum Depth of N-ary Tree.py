33682002cbf186e49913431ac57c1517cfdd242c
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2022-07-27 13:38:10 +0300

    S

--- leetcode/559. Maximum Depth of N-ary Tree.py
+++ leetcode/559. Maximum Depth of N-ary Tree.py
@@ -1,31 +1,59 @@
-#link https://leetcode.com/problems/maximum-depth-of-n-ary-tree/
+# link: https://leetcode.com/problems/maximum-depth-of-n-ary-tree/
+
+# DFS
 
 # time: O(n)
 # space: O(n)
 
-"""
-# Definition for a Node.
-class Node:
-    def __init__(self, val=None, children=None):
-        self.val = val
-        self.children = children
-"""
-
 class Solution:
     def maxDepth(self, root: 'Node') -> int:
         
-
-        if not root: return 0
+        
+        if not root:
+            return 0
         
         def dfs(root):
-            if root.children == None: return 1
+            
+            if root.children == None:
+                return 1
             
             depth = 0
-            
             for children in root.children:
                 depth = max(depth, dfs(children))
                 
             return depth + 1
 
         return dfs(root)
-    
+
+# BFS
+
+# time: O(n)
+# space: O(n)
+
+class Solution:
+    def maxDepth(self, root: 'Node') -> int:
+
+        depth = 0
+        queue = deque([root])
+        current_level_nodes = 1
+        next_level_nodes = 0
+
+        if not root:
+            return 0
+
+        while queue:
+
+            parent = queue.popleft()
+            current_level_nodes -= 1
+
+            for child in parent.children:
+                queue.append(child)
+                next_level_nodes += 1
+
+            if current_level_nodes == 0:
+                depth += 1
+                current_level_nodes = next_level_nodes
+                next_level_nodes = 0
+
+        return depth
+
