441085be975d16d4eb4dafc9436ee91c34777073
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2022-07-31 11:10:05 +0300

    s

---
+++ leetcode/547. Number of Provinces.py
@@ -0,0 +1,41 @@
+# link: https://leetcode.com/problems/number-of-provinces/submissions/
+
+# DFS
+
+# time: O(n^2)
+# space: O(n^2)
+
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        
+
+        n = len(isConnected)
+        visited = set()
+        province = 0
+        
+        def dfs(r):
+            
+            count = 0
+            
+            for c in range(n):
+                
+                if (r, c) in visited:
+                    continue
+                
+                visited.add((r, c))
+                
+                if isConnected[r][c] == 1:
+                    visited.add((c, r))
+                    count += 1
+                    dfs(c)
+            
+            return count
+                
+            
+        
+        for row in range(n):
+            province += dfs(row)
+            
+        return province
+
+        
