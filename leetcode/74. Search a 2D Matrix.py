1c1e1d0575f5a7ffbca009baf371b3140f52e6ab
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2022-08-05 21:54:55 +0300

    s

---
+++ leetcode/74. Search a 2D Matrix.py
@@ -0,0 +1,51 @@
+# Link: https://leetcode.com/problems/search-a-2d-matrix/
+
+# Binary search
+
+# Time: O(N * log(max - min))
+# Space: O(1)
+
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        
+        m = len(matrix)
+        n = len(matrix[0])
+        
+        start, end = matrix[0][0], matrix[m - 1][n - 1]
+        
+        def count_smaller(mid, m, n):
+            row, col = m - 1, 0
+            smaller_count = 0
+            
+            while row >= 0 and col < n:
+    
+                if matrix[row][col] > mid:
+                    bound[1] = min(bound[1], matrix[row][col])
+                    row -= 1
+                else:
+                    bound[0] = max(bound[0], matrix[row][col])
+                    smaller_count += 1
+                    col += 1
+                    
+            return smaller_count
+            
+        while start < end:
+            
+            mid = start + (end - start) // 2
+            
+            bound = [matrix[0][0], matrix[m - 1][n - 1]]
+            count = count_smaller(mid, m, n)
+            
+            if target <= mid:
+                end = bound[0]
+            if target > mid:
+                start = bound[1]
+        
+        if start == target or end == target:
+            return True
+        
+        return False
+                
+            
+            
