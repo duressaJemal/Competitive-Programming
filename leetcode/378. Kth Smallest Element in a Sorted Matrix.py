ef1557cbf0c1e52588b4915d57152bb3a6328807
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2022-08-05 14:37:15 +0300

    S

--- leetcode/378. Kth Smallest Element in a Sorted Matrix.py
+++ leetcode/378. Kth Smallest Element in a Sorted Matrix.py
@@ -1,9 +1,7 @@
-# Link: https://leetcode.com/problems/kth-smallest-element-in-a-sorted-matrix/
-
 # Heap
 
 # Time: O(k * log(N))  -- in the worst case k == N^2
-# Space: O(n^2)
+# Space: O(N^2)
 
 class Solution:
     def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
@@ -18,4 +16,53 @@ class Solution:
             
             heappop(heap)
             k -= 1
+      
+    
+# Binary search
+
+# Time: O(N * log(max - min))
+# Space: O(1)
+    
+class Solution:
+    def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
         
+        m = len(matrix)
+        n = len(matrix[0])
+        
+        def count_larger_smaller(matrix, mid, bound):
+            
+            smaller_count = 0
+            n = len(matrix)
+            row, col = len(matrix) - 1, 0
+            
+            while row >= 0 and col < n:
+                
+                if matrix[row][col] > mid:
+                    bound[1] = min(bound[1], matrix[row][col])
+                    row -= 1
+                
+                else:
+                    bound[0] = max(bound[0], matrix[row][col])
+                    smaller_count += row + 1
+                    col += 1
+            
+            return (smaller_count, bound)
+        
+        
+        start, end = matrix[0][0], matrix[m - 1][n - 1]
+        
+        while start < end:
+            
+            bound = [matrix[0][0], matrix[m - 1][n - 1]]
+            mid = start + (end - start) // 2
+
+            count, bound = count_larger_smaller(matrix, mid, bound)
+            if count == k:
+                return bound[0]
+            elif count < k:
+                start = bound[1]
+            else:
+                end = bound[0]
+        
+        return start
+                
