7d06b3afa735413acc69da0ac537817e18e6074c
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2022-08-09 19:01:14 +0300

    S

--- leetcode/130. Surrounded Regions.py
+++ leetcode/130. Surrounded Regions.py
@@ -1,98 +1,63 @@
 # link: https://leetcode.com/problems/surrounded-regions/
 
-# DFS
+# BFS
 
-# time: O(m*n)
-# space: O(m*n)
-
-class Solution:
-    def solve(self, board: List[List[str]]) -> None:
-        """
-        Do not return anything, modify board in-place instead.
-        """
-        
-        m = len(board)
-        n = len(board[0])
-        
-        def dfs(r, c):
-            
-            if r < 0 or c < 0 or r == m or c == n or board[r][c] != "O": 
-                return
-            
-            board[r][c] = "#"
-            
-            dfs(r + 1, c)
-            dfs(r - 1, c)
-            dfs(r, c + 1)
-            dfs(r, c - 1)
-        
-        for row in range(m):
-            for col in range(n):
-
-                if board[row][col] == "O" and (row in [0, m - 1] or col in [0, n - 1]):
-                    dfs(row, col)
-
-        for row in range(m):
-            for col in range(n):
-                
-                if board[row][col] == "O":
-                    board[row][col] = "X"
-                    
-        for row in range(m):
-            for col in range(n):
-                
-                if board[row][col] == "#":
-                    board[row][col] = "O"
-                    
-
-        
-# BFS 
-
-# time: O(m*n)
-# space: O(m*n)
+# Time: O(N)
+# Space: O(N)
 
 class Solution:
-    def solve(self, board: List[List[str]]) -> None:
+    def sumEvenGrandparent(self, root: TreeNode) -> int:
         
-        m = len(board)
-        n = len(board[0])
-        count = 0
-        queue = deque([])
-                    
-        for row in range(m):
-            for col in [0 ,n - 1]:
-                if board[row][col] == "O":
-                    board[row][col] = "#"
-                    queue.append((row, col))
+        SUM = 0
         
-        for row in [0, m - 1]:
-            for col in range(1, n - 1):
-                if board[row][col] == "O":
-                    board[row][col] = "#"
-                    queue.append((row, col))
+        queue = deque([(root, False)])
         
         while queue:
             
-            row, col = queue.popleft()
+            node, even_parent = queue.popleft()
             
-            for dirc in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
-                nr = row + dirc[0]
-                nc = col + dirc[1]
+            if even_parent:
+                SUM += node.left.val if node.left else 0
+                SUM += node.right.val if node.right else 0
+            
+            value = node.val % 2 == 0
+            
+            if node.left: 
+                queue.append((node.left, value))
+            if node.right: 
+                queue.append((node.right, value))
                 
-                if nr < 0 or nc < 0 or nr >= m or nc >= n:
-                    continue
-                
-                if board[nr][nc] == "O":
-                    board[nr][nc] = "#"
-                    queue.append((nr, nc))
-                
-        for row in range(m):
-            for col in range(n):
-                if board[row][col] == "O":
-                    board[row][col] = "X"
+            
+        return SUM
+    
+
+# DFS
+
+# Time: O(N)
+# Space: O(N)
+
+class Solution:
+    def sumEvenGrandparent(self, root: TreeNode) -> int:
         
-        for row in range(m):
-            for col in range(n):
-                if board[row][col] == "#":
-                    board[row][col] = "O"
-   
+        SUM = 0
+        
+        def dfs(node, even_parent):
+            
+            nonlocal SUM
+            
+            if not node:
+                return 
+            
+            if even_parent:
+                SUM += node.left.val if node.left else 0
+                SUM += node.right.val if node.right else 0
+            
+            value = node.val % 2 == 0
+            
+            dfs(node.left, value)
+            dfs(node.right, value)
+            
+        dfs(root, False)
+        
+        return SUM
+        
