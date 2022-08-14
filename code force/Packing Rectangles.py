ce2ae9b7de7b5d5719c49c19ce289911d71e784f
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2022-08-14 08:03:25 +0300

    s

---
+++ code force/Packing Rectangles.py
@@ -0,0 +1,27 @@
+# Link: https://codeforces.com/edu/course/2/lesson/6/2/practice/contest/283932/problem/A
+
+w, h, n = map(int, input().split())
+
+def is_good(x):
+    if (x // w) * (x // h) >= n:
+        return True
+    return False
+
+left = 0 # bad rectangle side
+right = 1 # good recangle side length
+while not is_good(right): right *= 2
+
+while right > left + 1:
+    mid = (left + right) // 2
+    if is_good(mid):
+        right = mid
+    else:
+        left = mid
+
+print(right)
+
+
+
+
+
+
