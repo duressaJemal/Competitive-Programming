09dc0a3744166fc0be0ff2e5303429bb3bd71007
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2022-09-16 09:54:09 +0300

    S

--- leetcode/64. Minimum Path Sum.py
+++ leetcode/64. Minimum Path Sum.py
@@ -1,5 +1,8 @@
 # link https://leetcode.com/problems/minimum-path-sum/
 
+# Time: O(M*N)
+# Space: O(M*N)
+
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
         
@@ -19,3 +22,25 @@ class Solution:
         return helper(0, 0)
         
         
+
+
+# Bottom-up
+
+# Time: O(M*N)
+# Space: O(M*N)
+
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        
+        m = len(grid)
+        n = len(grid[0])
+        
+        dp = [[0 for _ in range(n)] for _ in range(m)]
+        dp[0][0] = grid[0][0]
+
+        for i in range(m):
+            for j in range(n):
+                if i == j == 0: continue
+                dp[i][j] = min(dp[i - 1][j] if i > 0 else float("inf"), dp[i][j - 1] if j > 0 else float("inf")) + grid[i][j]
+        
+        return dp[m - 1][n - 1]
