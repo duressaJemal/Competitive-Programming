e488b39647af9132d69cd8583ab01a4b5c2c9468
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2022-09-16 07:34:51 +0300

    S

--- leetcode/509. Fibonacci Number.py
+++ leetcode/509. Fibonacci Number.py
@@ -1,7 +1,17 @@
+# Bottom-up
+
+# Time: O(N)
+# Space: O(1)
+
 class Solution:
     def fib(self, n: int) -> int:
+        left = 0
+        right = 1
         
-        if n == 0 or n == 1:
-            return n
-        return self.fib(n - 2) + self.fib(n - 1)
+        for i in range(2, n + 1):
+            temp = right
+            left = right + left
+            right, left = left, temp
+        
+        return 0 if not n else right
     
