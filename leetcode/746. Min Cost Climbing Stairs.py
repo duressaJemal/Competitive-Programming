cf59671570be9069d973868ef0ef0fcfeae0ac63
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2022-09-17 15:50:13 +0300

    S

--- leetcode/746. Min Cost Climbing Stairs.py
+++ leetcode/746. Min Cost Climbing Stairs.py
@@ -1,23 +1,5 @@
 # Link: https://leetcode.com/problems/min-cost-climbing-stairs/
 
-# Bottom-up
-
-# Time: O(N)
-# Space: O(N)
-
-class Solution:
-    def minCostClimbingStairs(self, cost: List[int]) -> int:
-        
-        n = len(cost)
-        dp = [0] * n
-        dp[0] = cost[0]
-        dp[1] = cost[1]
-            
-        for i in range(2, n):
-            dp[i] = min(dp[i - 1], dp[i - 2]) + cost[i]
-        
-        return min(dp[n - 1], dp[n - 2])
-    
 # Bottom-up(space-optimized)
 
 # Time: O(N)
@@ -37,3 +19,50 @@ class Solution:
         
         return min(left, right)
             
+        
+# Bottom-up
+
+# Time: O(N)
+# Space: O(N)
+
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        
+        n = len(cost)
+        dp = [0] * (n + 1)
+        dp[0], dp[1] = cost[0], cost[1]
+        
+        for i in range(2, n + 1):
+            dp[i] = min(dp[i - 1], dp[i - 2]) + (cost[i] if i < n else 0)
+        
+        return dp[n]
+        
+# Top-down
+
+# Time: O(N)
+# Space: O(N)
+
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        n = len(cost)
+        memo = {}
+        
+        def minCost(n):
+            
+            if n == 0 or n == 1: return cost[n]
+            if n in memo: return memo[n]
+            
+            memo[n] = min(minCost(n - 1), minCost(n - 2)) + (cost[n] if n < len(cost) else 0)
+            return memo[n]
+        
+        return minCost(n)
+            
+        
+
+        
+        
+        
+        
+        
+        
+        
