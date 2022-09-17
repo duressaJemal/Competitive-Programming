dbb364f457a1ab63f6dbec1014c00a4ed68c95c3
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2022-09-17 17:19:04 +0300

    S

--- leetcode/64. Minimum Path Sum.py
+++ leetcode/64. Minimum Path Sum.py
@@ -1,28 +1,4 @@
-# link https://leetcode.com/problems/minimum-path-sum/
-
-# Time: O(M*N)
-# Space: O(M*N)
-
-class Solution:
-    def minPathSum(self, grid: List[List[int]]) -> int:
-        
-        m = len(grid)
-        n = len(grid[0])
-        memo = {}
-        
-        def helper(r, c):
-            
-            if (r, c) in memo: return memo[(r, c)]
-            if r == m - 1 and c == n - 1: return grid[r][c]
-            if r >= m or c >= n: return float("inf")
-            
-            memo[(r, c)] =  grid[r][c] + min(helper(r + 1, c), helper(r, c + 1))
-            return memo[(r,c)]
-        
-        return helper(0, 0)
-        
-        
-
+# Link: https://leetcode.com/problems/minimum-path-sum/
 
 # Bottom-up
 
@@ -38,9 +14,40 @@ class Solution:
         dp = [[0 for _ in range(n)] for _ in range(m)]
         dp[0][0] = grid[0][0]
 
-        for i in range(m):
-            for j in range(n):
-                if i == j == 0: continue
-                dp[i][j] = min(dp[i - 1][j] if i > 0 else float("inf"), dp[i][j - 1] if j > 0 else float("inf")) + grid[i][j]
+        for i in range(1, m):
+            dp[i][0] = dp[i - 1][0] + grid[i][0]
+        
+        for j in range(1, n):
+            dp[0][j] = dp[0][j - 1] + grid[0][j]
+            
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j]
         
         return dp[m - 1][n - 1]
+
+# Top-down
+
+# Time: O(M*N)
+# Space: O(M*N)
+
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        
+        m = len(grid)
+        n = len(grid[0])
+        memo = {}
+        
+        def helper(r, c):
+            if r == 0 and c == 0: return grid[0][0]
+            if r < 0 or c < 0: return float("inf")
+            
+            if (r, c) in memo: return memo[(r, c)]
+            
+            memo[(r, c)] = min(helper(r - 1, c), helper(r, c - 1)) + grid[r][c]
+            return memo[(r, c)]
+        
+        return helper(m - 1, n - 1)
+        
+        
+        
