0086e190b90f73a398d63659989f9c09c2099bde
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2022-11-09 15:11:42 +0300

    S

--- leetcode/377. Combination Sum IV.py
+++ leetcode/377. Combination Sum IV.py
@@ -1,26 +1,46 @@
-# link https://leetcode.com/problems/combination-sum-iv/
+# Link: https://leetcode.com/problems/combination-sum-iv/
+#Q: 377. Combination Sum IV
+
+# Top-down:
+
+# Time: O(N)
+# Space: O(N)
 
 class Solution:
     def combinationSum4(self, nums: List[int], target: int) -> int:
         
+        dp = [-1] *(200_001)
         
-        memo = {}
-        def helper(target, nums):
-            
-            if target in memo: return memo[target]
-            if target == 0: return 1
-            if target < 0: return 0
-            
-            total = 0
-            for num in nums:
-                remainder = target - num
-                total += helper(remainder, nums)
-            
-            memo[target] = total
-            return total
-        
-        return helper(target, nums)
-
+        def calculate(s):
+            if s > target: return 0
+            if s == target: return 1
+            if dp[s] != -1: return dp[s]
+            res = 0
+            for x in nums:
+                res += calculate(s + x)
+            dp[s] = res
+            return dp[s]
     
+        return calculate(0)
 
-                
+# Bottom-up:
+
+# Time: O(N)
+# Space: O(N)
+
+class Solution:
+    def combinationSum4(self, nums: List[int], target: int) -> int:
+        
+        dp = [0] * (target + 1)
+        dp[0] = 1
+        
+        for i in range(1, target + 1):
+            for t in nums:
+                if t <= i:
+                    dp[i] += dp[i - t]
+                    
+        return dp[target]
+        
+        
+        
+        
