0242f5528d97262e5a5a0737df95886fb04ff569
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2022-11-18 10:05:12 +0300

    s

--- leetcode/1143. Longest Common Subsequence.py
+++ leetcode/1143. Longest Common Subsequence.py
@@ -1,78 +1,51 @@
-# link: https://leetcode.com/problems/longest-common-subsequence/
+# Link: https://leetcode.com/problems/longest-common-subsequence/
+#Q: 1143. Longest Common Subsequence
 
-# Top-down
-    
-    # time: O(n * m)
-    # space: O(n * m)
-    
+# Bottom-up
+
+# Time: O(M*N)
+# Space: O(M*N)
+
 class Solution:
-    
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
         
-        m = len(text1)
-        n = len(text2)
+        l1, l2 = len(text1), len(text2)
+        dp = [[0] * (l2 + 1)  for _ in range((l1 + 1))]
+        
+        for i in range(1, l1 + 1):
+            for j in range(1, l2 + 1):
+                if text1[i - 1] == text2[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1] + 1
+                else:
+                    dp[i][j] = max(dp[i][j - 1], dp[i - 1][j])
+        
+        return dp[l1][l2]
+    
+                
+# Top-down
+
+# Time: O(M*N) where M = len(text1), N = len(text2)
+# space: O(M*N)
+
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        
         memo = {}
-        
-        def lcf(x, y, m, n, memo):
-            if m <= 0 or n <= 0:
-                return 0
-            if (m , n) in memo:
-                return memo[(m, n)]
-            if x[m - 1] == y[n - 1]:
-                memo[(m, n)] = 1 + lcf(x, y, m - 1, n - 1, memo)
-                return memo[(m, n)]
+        def helper(i, j):
+            if i < 0 or j < 0: return 0
+            if i == 0 and j == 0:
+                if text1[i] == text2[j]: return 1
+                else: return 0
             
-            memo[(m, n)] =  max(lcf(x, y, m, n - 1, memo), lcf(x, y, m - 1, n, memo))
-            return memo[(m, n)]
-        return lcf(text1, text2, m, n, memo)
-
-# Bottom-up ----Not working
-# push dp
-
-# # time: O(m * n)
-# # space: O(m * n)
-
-# class Solution:
-#     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-
-#         m = len(text1)
-#         n = len(text2)
-#         dp = [[0] * (n + 1)] * (m + 1)
-        
-#         for i in range(m):
-#             for j in range(n):
-#                 if text1[i] == text2[j]:
-#                     dp[i + 1][j + 1] = dp[i][j] + 1
-#                 else:
-#                     dp[i + 1][j + 1] = max(dp[i + 1][j], dp[i][j + 1])
-#         return dp[m][n]
-
-
-# Bottom up
-# Pull dp
-
-# class Solution:
-#     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-
-#         m = len(text1)
-#         n = len(text2)
-        
-#         dp = [[0] * (n + 2)] * (m + 2)
-        
-#         for i in range(m + 1):
-#             for j in range(n + 1):
+            if (i, j) in memo: return memo[(i, j)]
+            if text1[i] == text2[j]:
+                memo[(i, j)] = helper(i - 1, j - 1) + 1
+            else:
+                memo[(i, j)] = max(helper(i - 1, j), helper(i, j - 1))
                 
-#                 if i == 0 or j == 0:
-#                     dp[i][j] == 0
+            return memo[(i, j)]
+        return helper(len(text1) - 1, len(text2) - 1)
                 
-#                 else:
-#                     dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
-#                     if text1[i - 1] == text2[j - 1]:
-#                         dp[i][j] = max(dp[i][j], dp[i-1][j-1] + 1)
-                    
-        
-#         print(dp)
-#         return dp[i][j]
-    
-    
 
+                
+                
