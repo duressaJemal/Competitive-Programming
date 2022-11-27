717ef92c2a1812b0edc959941abb56f8b952b51b
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2022-11-27 20:44:42 +0300

    S

--- leetcode/1609. Even Odd Tree.py
+++ leetcode/1609. Even Odd Tree.py
@@ -1,4 +1,3 @@
-
 # Link: https://leetcode.com/problems/even-odd-tree/
 #Q: 1609. Even Odd Tree
 
@@ -11,33 +10,28 @@ class Solution:
         queue = deque([root])
         level = -1
         while queue:
-            n, store = len(queue), []
             level += 1
+            n, prev = len(queue), 0 if level % 2 == 0 else float("inf")
             for i in range(n):
                 root = queue.popleft()
-                store.append(root.val)
-                
+                if level % 2 == 0:
+                    if root.val <= prev or root.val % 2 == 0: return False
+                else:
+                    if root.val >= prev or root.val % 2 != 0: return False
+                    
+                prev = root.val
                 if root.left:
                     queue.append(root.left)
                 if root.right:
                     queue.append(root.right)
                 
-            # check all conditions
-            if level % 2 == 0:
-                comp = 0
-                for i in range(len(store)):
-                    if store[i] % 2 == 0 or store[i] <= comp:
-                        return False
-                    comp = store[i] 
-            else:
-                comp = float("inf")
-                for i in range(len(store)):
-                    if store[i] % 2 != 0 or store[i] >= comp:
-                        return False
-                    comp = store[i]
         return True
                 
             
+                
+                
+                
+        
                 
                 
                 
