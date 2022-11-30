445ec4b0b5fb5be04c520e5cec8b95bd99c9c9e1
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2022-11-30 18:14:33 +0300

    S

--- leetcode/236. Lowest Common Ancestor of a Binary Tree.py
+++ leetcode/236. Lowest Common Ancestor of a Binary Tree.py
@@ -1,7 +1,7 @@
 
-# Link: https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
 
-# DFS
+# Link: https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
+#Q: 236. Lowest Common Ancestor of a Binary Tree
 
 # Time: O(N)
 # Space: O(N)
@@ -9,23 +9,19 @@
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         
-        lca_node = None
+        self.output = root
+        def dfs(root):
+            
+            if not root: return root
+            left = dfs(root.left)
+            right = dfs(root.right)
         
-        def dfs(node):
-            
-            nonlocal lca_node
-            
-            if not node or lca_node != None: return False
-            left = dfs(node.left)
-            right = dfs(node.right)
-            
-            current = node == p or node == q
+            current = root.val == p.val or root.val == q.val
             
             if (current and left) or (current and right) or (left and right):
-                lca_node = node
-                
+                self.output = root
+                return True
             return current or left or right
-        
+
         dfs(root)
-        
-        return lca_node
+        return self.output
