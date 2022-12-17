8a9cbaf7d23932f202f513e55ae8b433792b91be
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2022-12-17 20:22:29 +0300

    S

--- leetcode/1991. Find the Middle Index in Array.py
+++ leetcode/1991. Find the Middle Index in Array.py
@@ -1,13 +1,19 @@
+# Link: https://leetcode.com/problems/find-the-middle-index-in-array/
+#Q: 1991. Find the Middle Index in Array
+
+# Time: O(N)
+# Space: O(N)
+
 class Solution:
     def findMiddleIndex(self, nums: List[int]) -> int:
+        n = len(nums)
+        prefix = [0] * (n + 1)
         
-        prefix = [0] * (len(nums) + 1)
+        for i in range(1, n + 1):
+            prefix[i] = prefix[i - 1] + nums[i - 1]
         
-        for i in range(1, len(nums) + 1):
-            prefix[i] = prefix[i-1] + nums[i-1]
-        
-        for i in range(1, len(prefix)):
-            if prefix[i-1] == prefix[-1] - prefix[i]:
-                return i-1
+        for i in range(1, n + 1):
+            if prefix[i - 1] == prefix[-1] - prefix[i]:
+                return i - 1
             
-        return - 1
+        return -1
