0ad7379d1128b75c2dd463fa71ec2a8366855112
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2022-12-17 19:47:07 +0300

    S

--- leetcode/1588. Sum of All Odd Length Subarrays.py
+++ leetcode/1588. Sum of All Odd Length Subarrays.py
@@ -1,57 +1,47 @@
-# link: https://leetcode.com/problems/sum-of-all-odd-length-subarrays/
+# Link: https://leetcode.com/problems/sum-of-all-odd-length-subarrays/
+#Q: 1588. Sum of All Odd Length Subarrays
 
-# Naive
+# Time: O(N^2)
+# Space: O(N)
 
-# time: O(n^3)
-# space: O(n)
+class Solution:
+    def sumOddLengthSubarrays(self, arr: List[int]) -> int:
+        n = len(arr)
+        prefix = [0] * (n + 1)
+        
+        # Build prefix
+        for i in range(1, n + 1):
+            prefix[i] = prefix[i - 1] + arr[i - 1]
+        
+        total = 0
+        for i in range(1, n + 1, 2):
+            l, r = 0, i
+            while r <= n:
+                total += (prefix[r] - prefix[l])
+                l += 1
+                r += 1
+                
+        return total
+
+# Time: O(N^2)
+# Space: O(1)
 
 class Solution:
     def sumOddLengthSubarrays(self, arr: List[int]) -> int:
         
         n = len(arr)
-        SUM = 0
+        output = 0
         
-        for length in range(1, n + 1, 2):
-            
-            end = length
-            start = 0
+        for l in range(n):
+            current = 0
+            for r in range(l, n):
+                current += arr[r]
+                if (r - l) % 2 == 0:
+                    output += current
+        return output
 
-            while end <= n:
-                SUM += sum(arr[start: end])
-                start += 1
-                end  += 1
-            
-        return SUM
-                
-                
-                
-# Prefix Sum
 
-# time: O(n ^ 2)
-# space: O(n)
-
-class Solution:
-    def sumOddLengthSubarrays(self, arr: List[int]) -> int:
         
-        n = len(arr)
-        prefix = [0] * n
-        prefix[0] = arr[0]
-        
-        for i in range(1, n):
-            prefix[i] = prefix[i - 1] + arr[i]
-        
-        answer = 0
-        for i in range(n):
-            end = i
-            while end < n:
-                if i == 0:
-                    answer += prefix[end]
-                else:
-                    answer += prefix[end] - prefix[i - 1]
-                
-                end += 2
-        
-        return answer
          
         
 # Optimized
