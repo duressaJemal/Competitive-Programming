ad1be9674f33b4f86a2eb46a1f7c5519e3711a82
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2022-12-19 23:21:09 +0300

    S

--- leetcode/1413. Minimum Value to Get Positive Step by Step Sum.py
+++ leetcode/1413. Minimum Value to Get Positive Step by Step Sum.py
@@ -1,10 +1,19 @@
+# Link: https://leetcode.com/problems/minimum-value-to-get-positive-step-by-step-sum/
+#Q: 1413. Minimum Value to Get Positive Step by Step Sum
+
+# Time: O(N)
+# Space: O(1)
+
 class Solution:
     def minStartValue(self, nums: List[int]) -> int:
         
-        for i in range(1, len(nums)):
-            nums[i] += nums[i - 1]
-            
-        minimum = min(nums)
+        n = len(nums)
+        mn = nums[0]
         
-        return 1 if minimum >= 1 else 0 - minimum + 1
-    
+        for i in range(1, n):
+            nums[i] += nums[i - 1]
+            mn = min(mn, nums[i])
+        
+        if mn >= 0: return 1
+        if mn < 0: return 0 - (mn - 1)
+        
