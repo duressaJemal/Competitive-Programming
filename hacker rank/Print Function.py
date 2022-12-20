9094985c179e2b37b08406316b46496c1313f9dd
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2022-12-20 21:58:02 +0300

    S

---
+++ hacker rank/Print Function.py
@@ -0,0 +1,9 @@
+# Link: https://www.hackerrank.com/challenges/python-print/problem?isFullScreen=false
+#Q: Print Function
+
+if __name__ == '__main__':
+    n = int(input())
+
+for i in range(1, n + 1):
+    print(i, end = "")
+    
