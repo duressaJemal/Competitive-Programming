4978fdbc80eace3dd24ef2e415991cca6c91c034
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2022-12-20 18:22:36 +0300

    S

--- leetcode/9. Palindrome Number.py
+++ leetcode/9. Palindrome Number.py
@@ -1,13 +1,24 @@
-#link https://leetcode.com/problems/palindrome-number/
+# Link: https://leetcode.com/problems/palindrome-number/
+#Q: 9. Palindrome Number
+
+# Time: O(log10(N))
+# Space: O(1)
+
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         
         if x < 0 or (x % 10 == 0 and x != 0):
             return False
         
-        generated = 0
-        while x > generated:
-            generated = generated * 10 + x % 10
-            x = x // 10
+        if 0 <= x <= 9: return True
         
-        return x == generated or x == generated // 10
+        original = x
+        
+        generated = 0
+        while x > 0:
+            generated = generated * 10 + (x % 10)
+            x //= 10
+        return generated == original
+        
+        
+        
