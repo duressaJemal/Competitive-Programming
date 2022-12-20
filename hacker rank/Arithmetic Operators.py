e520dd43d31611be006a78de531a56d5fe7db9cf
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2022-12-20 21:45:07 +0300

    S

---
+++ hacker rank/Arithmetic Operators.py
@@ -0,0 +1,10 @@
+# Link: https://www.hackerrank.com/challenges/python-arithmetic-operators/problem?isFullScreen=false
+#Q: Arithmetic Operators
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+print(a + b)
+print(a - b)
+print(a * b)
