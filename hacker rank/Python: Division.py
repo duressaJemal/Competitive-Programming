18304c93de1d8695221ba68a6e7aab74a1b36e53
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2022-12-20 21:52:40 +0300

    S

---
+++ hacker rank/Python: Division.py
@@ -0,0 +1,10 @@
+# Link: https://www.hackerrank.com/challenges/python-division/problem
+#Q: Python: Division
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+print(a//b)
+print(a/b)
+
