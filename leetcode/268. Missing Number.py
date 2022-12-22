d3a662a9f387a671d39465b6cdd6c27a5e9444c7
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2022-12-22 12:49:37 +0300

    S

--- leetcode/268. Missing Number.py
+++ leetcode/268. Missing Number.py
@@ -1,7 +1,11 @@
-#link https://leetcode.com/problems/missing-number/
+# Link: https://leetcode.com/problems/missing-number/
+#Q: 268. Missing Number
+
+# Time: O(N)
+# Space: O(1)
 
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-    
+        
         n = len(nums)
-        return (n * (n + 1))//2 - sum(nums)
+        return (n * (n + 1)) // 2 - sum(nums)
