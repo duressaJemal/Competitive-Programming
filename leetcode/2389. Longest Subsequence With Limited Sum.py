6abd5e84c5fdc1d87e59458eadbae12b096352cc
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2022-12-22 09:58:49 +0300

    S

---
+++ leetcode/2389. Longest Subsequence With Limited Sum.py
@@ -0,0 +1,47 @@
+# Link: https://leetcode.com/problems/longest-subsequence-with-limited-sum/
+#Q: 2389. Longest Subsequence With Limited Sum
+
+# Time: O(M*N)
+# Space: O(M)
+
+class Solution:
+    def answerQueries(self, nums: List[int], queries: List[int]) -> List[int]:
+        
+        ans = [0] * len(queries)
+        nums.sort()
+        for l_n in range(1, len(nums)):
+            nums[l_n] += nums[l_n - 1]
+        
+        for l_q in range(len(queries)):
+            for l_n in range(len(nums)):
+                if nums[l_n] <= queries[l_q]:
+                    ans[l_q] = l_n + 1
+        return ans
+        
+    
+# Time: O(M*log(N))
+# Space: O(M + N)
+
+class Solution:
+    def answerQueries(self, nums: List[int], queries: List[int]) -> List[int]:
+        
+        ans = [0] * len(queries)
+        nums.sort()
+        for l_n in range(1, len(nums)):
+            nums[l_n] += nums[l_n - 1]
+        
+        for l_q in range(len(queries)):
+            # binary search
+            l = -1
+            r = len(nums) # assume good value
+            
+            while r > l + 1:
+                mid = (l + r) // 2
+                if nums[mid] > queries[l_q]:
+                    r = mid
+                else:
+                    l = mid
+                    
+            ans[l_q] = r
+        
+        return ans
