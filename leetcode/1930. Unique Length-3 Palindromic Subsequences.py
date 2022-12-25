5c5d39d64025f772d94c8d8e083826ffbf960235
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2022-12-25 12:54:05 +0300

    S

--- leetcode/1930. Unique Length-3 Palindromic Subsequences.py
+++ leetcode/1930. Unique Length-3 Palindromic Subsequences.py
@@ -1,54 +1,46 @@
 # Link: https://leetcode.com/problems/unique-length-3-palindromic-subsequences/
 #Q: 1930. Unique Length-3 Palindromic Subsequences
 
-# Time: O(N*26)
+# Time: O(N*26) == O(N)
 # Space: O(N)
 
 class Solution:
     def countPalindromicSubsequence(self, s: str) -> int:
+
+        n = len(s)
+        prefix = [[0 for _ in range(26)] for _ in range(n)]
+        prefix[0][ord(s[0]) - ord("a")] = 1
         
-        last_index = defaultdict(int)
+        # build 2D prefix
+        for i in range(1, n):
+            current = prefix[i - 1].copy()
+            current[ord(s[i]) - ord("a")] += 1
+            prefix[i] = current.copy()
+
+        # count the start and end index for each character
+        range_list = defaultdict(list)
         for index, value in enumerate(s):
-            last_index[value] = index
+            if not range_list[value]:
+                range_list[value] = [index, index]
+            else:
+                range_list[value][1] = index
         
-        output = set()
-        visited = set()
-        
-        for i in range(len(s)):
-            if s[i] not in visited:
-                if len(visited) == 26:
-                    break
-                start = i
-                end = last_index[s[i]]
-                for j in range(i + 1, end):
-                    output.add(s[start] + s[j] + s[end])
-                visited.add(s[i])
+        count = 0
+        # iterate over each char of the string
+        for key in range_list:
+            left_index = range_list[key][0]
+            right_index = range_list[key][1]
+            
+            if right_index > left_index + 1:
+                for i in range(26):
+                    unique = prefix[right_index - 1][i] - prefix[left_index][i]
+                    if unique:
+                        count += 1
+        return count
+            
+            
+            
+                                                           
                 
-        return len(output)
-     
-        
-# Wrong Answer
-
-#         n = len(s)
-#         prefix = [[0 for _ in range(26)] for _ in range(n)]
-#         prefix[0][ord("a") - ord(s[0])] = 1
-        
-#         for i in range(1, n):
-#             current = prefix[i - 1].copy()
-#             current[ord(s[i]) - ord("a")] += 1
-#             prefix[i] = current.copy()
-        
-#         output = set()
-        
-#         for i in range(1, n - 1):
-#             for j in range(26):
-
-#                 before_current = prefix[i - 1][j]
-#                 after_current = prefix[-1][j] - prefix[i][j]
-
-#                 if before_current and after_current:
-#                     output.add(chr(ord("a") + j) + s[i] + chr(ord("a") + j))
-
-#         return len(output)
                 
             
