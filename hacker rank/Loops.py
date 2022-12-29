824216002d5f48f13b5a2a0df3b5442dda100759
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2022-12-29 20:33:17 +0300

    S

---
+++ hacker rank/Loops.py
@@ -0,0 +1,8 @@
+# Link: https://www.hackerrank.com/challenges/python-loops/problem?isFullScreen=false
+#Q: Loops
+
+if __name__ == '__main__':
+    n = int(input())
+
+for i in range(n):
+    print(i * i)
