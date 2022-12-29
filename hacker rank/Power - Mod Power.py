f6c2ddb819505ef4294880d7911048b08b9224c4
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2022-12-29 20:37:21 +0300

    S

---
+++ hacker rank/Power - Mod Power.py
@@ -0,0 +1,10 @@
+# Link: https://www.hackerrank.com/challenges/python-power-mod-power/problem?isFullScreen=false
+#Q: Power - Mod Power
+
+a = int(input())
+b = int(input())
+m = int(input())
+
+power = a ** b
+print(power)
+print(power % m)
