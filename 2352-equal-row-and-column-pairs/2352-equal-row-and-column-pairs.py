df2f32d56f514618b2bcdd91b8f525083a4d5aa1
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2023-01-05 22:57:38 +0300

    Time: 1279 ms (50.32%), Space: 19.6 MB (7.59%) - LeetHub

---
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
@@ -0,0 +1,36 @@
+class Solution:
+    def equalPairs(self, grid: List[List[int]]) -> int:
+        
+        row_elements = {}
+        col_elements = {}
+        
+        row_len = len(grid[0])
+        col_len = row_len
+        
+        for index, row in enumerate(grid):
+            row_elements[index] = tuple(row)
+            
+        for col in range(col_len):
+            temporary = []
+            for row in range(row_len):
+                temporary.append(grid[row][col])
+            
+            col_elements[col] = tuple(temporary)
+        
+        
+        print(row_elements)
+        print(col_elements)
+        equal_pairs = 0
+        
+        for row in range(row_len):
+            for col in range(col_len):
+                if row_elements[row] == col_elements[col]:
+                    equal_pairs += 1
+        
+        return equal_pairs
+                
+        
+        
+        
+        
+        
