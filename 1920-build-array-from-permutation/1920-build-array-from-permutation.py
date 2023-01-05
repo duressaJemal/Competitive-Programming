280379e87f20bcd8fba411b977471da4d18908e8
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2023-01-05 22:30:37 +0300

    Time: 347 ms (7.51%), Space: 14.2 MB (8.10%) - LeetHub

--- 1920-build-array-from-permutation/1920-build-array-from-permutation.py
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.py
@@ -1,3 +1,6 @@
+# Time: O(N)
+# Space: O(N)
+
 class Solution:
     def buildArray(self, nums: List[int]) -> List[int]:
         
@@ -7,3 +10,20 @@ class Solution:
             ans[index] = nums[value]
         
         return ans
+
+# follow-up (from discuss section)
+
+# Time: O(N)
+# Space: O(1)
+
+class Solution:
+    def buildArray(self, nums: List[int]) -> List[int]:
+        
+        n = len(nums)
+        for index in range(n):
+            nums[index] = nums[index] + n*(nums[nums[index]] % n)
+        
+        for index, value in enumerate(nums):
+            nums[index] = value // n
+        
+        return nums
