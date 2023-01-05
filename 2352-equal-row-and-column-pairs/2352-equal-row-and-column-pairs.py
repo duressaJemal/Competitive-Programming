5e1e2179a29efebc00c59de1149741d89747900a
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2023-01-05 22:58:43 +0300

    Time: 1916 ms (19.63%), Space: 18.8 MB (92.09%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
@@ -17,8 +17,8 @@ class Solution:
             
             col_elements[col] = tuple(temporary)
         
-        equal_pairs = 0
         
+        equal_pairs = 0
         for row in range(row_len):
             for col in range(col_len):
                 if row_elements[row] == col_elements[col]:
