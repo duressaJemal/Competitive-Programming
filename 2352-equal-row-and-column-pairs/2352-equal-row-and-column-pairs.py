cac839a8e6abe0064a5420e0013b7573917f201d
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2023-01-06 22:09:04 +0300

    Time: 1884 ms (19.94%), Space: 19 MB (29.91%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
@@ -1,3 +1,6 @@
+# Time: O(N^3)
+# Space: O(N^2)
+
 class Solution:
     def equalPairs(self, grid: List[List[int]]) -> int:
         
@@ -7,16 +10,17 @@ class Solution:
         row_len = len(grid[0])
         col_len = row_len
         
+        # store all row values 
         for index, row in enumerate(grid):
             row_elements[index] = tuple(row)
-            
+           
+        # store all column values
         for col in range(col_len):
             temporary = []
             for row in range(row_len):
                 temporary.append(grid[row][col])
             
             col_elements[col] = tuple(temporary)
-        
         
         equal_pairs = 0
         for row in range(row_len):
