1d15413613ebd735c48144c2068fcdbd46a4d8f1
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2023-01-09 08:44:38 +0300

    Time: 1501 ms (80.26%), Space: 31.7 MB (9.46%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.py
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.py
@@ -1,19 +1,62 @@
 # Time: O(N)
 # Space: O(N)
 
+# class Solution:
+#     def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
+        
+#         before_pivot = []
+#         after_pivot = []
+        
+#         count = 0
+        
+#         for index, value in enumerate(nums):
+#             if value < pivot:
+#                 before_pivot.append(value)
+#             elif value > pivot:
+#                 after_pivot.append(value)
+#             else:
+#                 count += 1
+#         return before_pivot + [pivot] * count + after_pivot
+    
+    
+# optimized
+
+# Time: O(N)
+# Space: O(N)
+
 class Solution:
     def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
+     
+        n = len(nums)
+        output = [0] * n
         
-        before_pivot = []
-        after_pivot = []
-        
-        count = 0
+        low = 0
+        similar = 0
+        high = 0
         
         for index, value in enumerate(nums):
             if value < pivot:
-                before_pivot.append(value)
+                low += 1
+            elif value == pivot:
+                similar += 1
+        
+        high = low + similar
+        similar = low
+        low = 0
+        
+        for index, value in enumerate(nums):
+            if value < pivot:
+                output[low] = value
+                low += 1
             elif value > pivot:
-                after_pivot.append(value)
+                output[high] = value
+                high += 1
             else:
-                count += 1
-        return before_pivot + [pivot] * count + after_pivot
+                output[similar] = value
+                similar += 1
+        
+        return output
+        
+
+
+
