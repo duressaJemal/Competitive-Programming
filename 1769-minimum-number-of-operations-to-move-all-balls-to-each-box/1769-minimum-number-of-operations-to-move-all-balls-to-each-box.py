79cc4eb1c113f6f50cfc7795f184f937bfdb47ef
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2023-01-10 18:45:28 +0300

    Time: 74 ms (89.08%), Space: 14.2 MB (85.53%) - LeetHub

--- 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.py
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.py
@@ -1,21 +1,21 @@
 # # Time: O(N^2)
 # # Space: O(N)
 
-# class Solution:
-#     def minOperations(self, boxes: str) -> List[int]:
+class Solution:
+    def minOperations(self, boxes: str) -> List[int]:
         
-#         n = len(boxes)
-#         output = []
+        n = len(boxes)
+        output = []
         
-#         for index, value in enumerate(boxes):
-#             operations = 0
-#             for i in range(n):
-#                 if boxes[i] == "1":
-#                     operations += abs(index - i)
+        for index, value in enumerate(boxes):
+            operations = 0
+            for i in range(n):
+                if boxes[i] == "1":
+                    operations += abs(index - i)
             
-#             output.append(operations)
+            output.append(operations)
         
-#         return output
+        return output
 
     
 # Time: O(N)
