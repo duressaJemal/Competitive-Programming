a47ae02cfd6df99d9c8e91edf8a24c7fe1a8c66b
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2023-01-12 19:33:40 +0300

    Time: 55 ms (55.44%), Space: 14.4 MB (87.56%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -3,33 +3,33 @@
 # Time: O(log(M*N))
 # Space: O(1)
 
-# class Solution:
-#     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         
-#         for row in matrix:
+        for row in matrix:
             
-#             is_found = False
+            is_found = False
             
-#             # do binary search
+            # do binary search
             
-#             left = -1
-#             right = len(row) - 1
+            left = -1
+            right = len(row) - 1
             
-#             while left + 1 < right:
+            while left + 1 < right:
                 
-#                 mid = (left + right) // 2
-#                 if target > row[mid]:
-#                     left = mid
-#                 elif target < row[mid]:
-#                     right = mid
-#                 else:
-#                     is_found = True
-#                     break
+                mid = (left + right) // 2
+                if target > row[mid]:
+                    left = mid
+                elif target < row[mid]:
+                    right = mid
+                else:
+                    is_found = True
+                    break
             
-#             if is_found or row[right] == target:
-#                 return True
+            if is_found or row[right] == target:
+                return True
         
-#         return False
+        return False
                 
 # optimized
 
@@ -38,38 +38,6 @@
 
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        
-        """
-        
-        start from the top
-        go to down or right:
-            if current grid is the value:
-                return True
-                
-            if larger or equal to downward value:
-                go downward:
-            elif >= right value:
-                go to the right
-            else:
-                return False
-        
-        how to implement it?
-        
-        iterate over M + N:
-            and hold current_row and current_column
-            if there is way to move to left or down:
-                pass
-            else:
-                we have reached the last element
-                
-            if there is no way to move downward:
-                move to the right
-            
-            if there is no way to move righwards:
-                move downward
-            
-            
-        """
         
         m = len(matrix)
         n = len(matrix[0])
