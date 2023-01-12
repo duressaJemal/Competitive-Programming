4ae99916d6479e466a2be73aea03edebaff12794
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2023-01-12 07:56:28 +0300

    Time: 49 ms (95.57%), Space: 14.3 MB (20.19%) - LeetHub

--- 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.py
+++ 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.py
@@ -8,22 +8,11 @@ class Solution:
                 nums[index] *= 2
                 nums[index + 1] = 0
         
-        
         write = 0
-        
         for read in range(n):
-            
-            if not nums[read]:
-                
-                # find write index
-                while write < read or (write < n and not nums[write]):
-                    write += 1
-                
-                if write < n:
-                    nums[read], nums[write] = nums[write], nums[read]
-                else:
-                    return nums
-        
+            if nums[read]:
+                nums[read], nums[write] = nums[write], nums[read]
+                write += 1
         return nums
         
         
