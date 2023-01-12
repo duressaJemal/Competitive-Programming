a757888f5f8b27c835d8a9a62bc4c2e9827d4d65
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2023-01-12 06:45:59 +0300

    Time: 61 ms (67.48%), Space: 13.9 MB (95.09%) - LeetHub

---
+++ 1002-find-common-characters/1002-find-common-characters.py
@@ -0,0 +1,24 @@
+class Solution:
+    def commonChars(self, words: List[str]) -> List[str]:
+        
+        n = len(words)
+        
+        total_counter = [100] * 26
+        output = []
+        
+        ofset = ord("a")
+        
+        for word in words:
+            counter = [0] * 26
+            for char in word:
+                asci = ord(char)
+                counter[asci - ofset] += 1
+            
+            for index, value in enumerate(counter):
+                total_counter[index] = min(total_counter[index], value)
+                
+        for index, value in enumerate(total_counter):
+            for i in range(value):
+                output.append(chr(index + ofset))
+        
+        return output
