dbc80b658faf5e16aa1022ab82bb815e8e52b52d
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2023-01-13 18:01:46 +0300

    Time: 92 ms (73.27%), Space: 13.9 MB (75.05%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.py
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.py
@@ -1,22 +1,22 @@
 # Time: O(M*N)
 # Space: O(M*N)
 
-# class Solution:
-#     def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
+class Solution:
+    def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
         
-#         digonal_values = defaultdict(int)
+        digonal_values = defaultdict(int)
         
-#         row_len = len(matrix)
-#         col_len = len(matrix[0])
+        row_len = len(matrix)
+        col_len = len(matrix[0])
         
-#         for row in range(row_len):
-#             for col in range(col_len):
-#                 digonal_values[row - col].add(matrix[row][col])
+        for row in range(row_len):
+            for col in range(col_len):
+                digonal_values[row - col].add(matrix[row][col])
         
-#         for value in digonal_values.values():
-#             if len(value) > 1:
-#                 return False
-#         return True
+        for value in digonal_values.values():
+            if len(value) > 1:
+                return False
+        return True
             
 
 # optimized
