3753a0ed5c3ca8e273697381c30bacc8412616dd
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2023-01-13 18:29:57 +0300

    Time: 135 ms (78.48%), Space: 14.6 MB (94.53%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -0,0 +1,37 @@
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        
+        """
+        change the changable characters into temporaray char without changing the zeros
+        
+        then change all the temp characters into zeros 
+        
+        """
+        
+        row_len = len(matrix)
+        col_len = len(matrix[0])
+        
+        for row in range(row_len):
+            for col in range(col_len):
+                
+                if matrix[row][col] == 0:
+                    # change it's row and col neighbours into temporary "#"
+                    for r in range(row_len):
+                        if matrix[r][col] != 0:
+                            matrix[r][col] = "#"
+                    
+                    for c in range(col_len):
+                        if matrix[row][c] != 0:
+                            matrix[row][c] = "#"
+        
+        for row in range(row_len):
+            for col in range(col_len):
+                if matrix[row][col] == "#":
+                    matrix[row][col] = 0
+        
+        
+        
+                    
