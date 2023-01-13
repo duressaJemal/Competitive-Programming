edd03bbfce386f4edf271bf7ef53927dd9340e71
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2023-01-13 09:59:36 +0300

    Time: 43 ms (90.49%), Space: 14.3 MB (99.55%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,39 +1,6 @@
-# binary search
+# Follow-up
 
-# Time: O(M*log(N))
-# Space: O(1)
-
-class Solution:
-    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        
-        for row in matrix:
-            
-            is_found = False
-            
-            # do binary search
-            
-            left = -1
-            right = len(row) - 1
-            
-            while left + 1 < right:
-                
-                mid = (left + right) // 2
-                if target > row[mid]:
-                    left = mid
-                elif target < row[mid]:
-                    right = mid
-                else:
-                    is_found = True
-                    break
-            
-            if is_found or row[right] == target:
-                return True
-        
-        return False
-                
-# optimized
-
-# Time: O(M + N)
+# Time: O(log(M*N))
 # Space: O(1)
 
 class Solution:
@@ -42,23 +9,85 @@ class Solution:
         m = len(matrix)
         n = len(matrix[0])
         
-        row = 0
-        col = 0
+        left = 0
+        right = m * n
         
-        for operation in range(m + n):
+        while left + 1 < right:
             
-            # if either is valid
-            if row < m and col < n:
+            mid = (left + right) // 2
+            row = mid // n
+            col = mid % n
+            
+            if target == matrix[row][col]:
+                return True
+            elif target > matrix[row][col]:
+                left = mid
+            else:
+                right = mid
+            
+        return target == matrix[left // n][left % n]
+    
+# binary search
+
+# Time: O(M*log(N))
+# Space: O(1)
+
+# class Solution:
+#     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        
+#         for row in matrix:
+            
+#             is_found = False
+            
+#             # do binary search
+            
+#             left = -1
+#             right = len(row) - 1
+            
+#             while left + 1 < right:
                 
-                if target == matrix[row][col]:
-                    return True
+#                 mid = (left + right) // 2
+#                 if target > row[mid]:
+#                     left = mid
+#                 elif target < row[mid]:
+#                     right = mid
+#                 else:
+#                     is_found = True
+#                     break
+            
+#             if is_found or row[right] == target:
+#                 return True
+        
+#         return False
                 
-                if row + 1 < m and target >= matrix[row + 1][col]:
-                    row += 1
-                elif col + 1 < n and target >= matrix[row][col + 1]:
-                    col += 1
-                else:
-                    return False
+# optimized
+
+# Time: O(M + N)
+# Space: O(1)
+
+# class Solution:
+#     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        
+#         m = len(matrix)
+#         n = len(matrix[0])
+        
+#         row = 0
+#         col = 0
+        
+#         for operation in range(m + n):
+            
+#             # if either is valid
+#             if row < m and col < n:
+                
+#                 if target == matrix[row][col]:
+#                     return True
+                
+#                 if row + 1 < m and target >= matrix[row + 1][col]:
+#                     row += 1
+#                 elif col + 1 < n and target >= matrix[row][col + 1]:
+#                     col += 1
+#                 else:
+#                     return False
         
         
 
