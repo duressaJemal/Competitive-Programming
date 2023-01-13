0edb3ee4ab8423d667fcb1627f7fc8984373f915
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2023-01-13 18:01:30 +0300

    Time: 89 ms (82.77%), Space: 13.8 MB (75.05%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.py
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.py
@@ -1,17 +1,48 @@
+# Time: O(M*N)
+# Space: O(M*N)
+
+# class Solution:
+#     def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
+        
+#         digonal_values = defaultdict(int)
+        
+#         row_len = len(matrix)
+#         col_len = len(matrix[0])
+        
+#         for row in range(row_len):
+#             for col in range(col_len):
+#                 digonal_values[row - col].add(matrix[row][col])
+        
+#         for value in digonal_values.values():
+#             if len(value) > 1:
+#                 return False
+#         return True
+            
+
+# optimized
+
+# Time: O(M*N)
+# Space: O(1)
+
 class Solution:
     def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
-        
-        digonal_values = defaultdict(set)
         
         row_len = len(matrix)
         col_len = len(matrix[0])
         
-        for row in range(row_len):
-            for col in range(col_len):
-                digonal_values[row - col].add(matrix[row][col])
+        for row in range(row_len - 1):
+            for col in range(col_len - 1):
+                if matrix[row][col] != matrix[row + 1][col + 1]:
+                    return False
         
-        for value in digonal_values.values():
-            if len(value) > 1:
-                return False
         return True
-                
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
