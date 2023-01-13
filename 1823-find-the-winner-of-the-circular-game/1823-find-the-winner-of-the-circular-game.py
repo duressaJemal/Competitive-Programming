6565db7a4e316c77719abd0142dc717dd54ed3c9
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2023-01-13 19:38:36 +0300

    Time: 35 ms (89.25%), Space: 13.9 MB (42.66%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
@@ -1,28 +1,8 @@
+# Time: O(N^2)
+# Space: O(N)
+
 class Solution:
     def findTheWinner(self, n: int, k: int) -> int:
-        
-        """
-        start from current
-        
-        
-        500 * 500 = 250, 000
-        
-        current_index = 
-        
-        1, 2, 3, 4, 5,
-        
-        4 % 4 = 0
-        
-        if it was on 0 then it will be on 0
-        but if to the right:
-            
-          
-        current_index = (current_index + k) % len(array)
-        remove from current_index
-        then calibrate the new index
-            new_index = current_index % len(array)
-        
-        """
         
         nums = list(range(1, n + 1))
         
@@ -34,8 +14,8 @@ class Solution:
             current_index = remove_index % len(nums)
         
         return nums[0]
-    
-        
+
+
         
         
     
