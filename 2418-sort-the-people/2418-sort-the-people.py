6ed11e80fec7f6e0db77043165ba1d62c439c6a7
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2023-01-17 18:27:40 +0300

    Time: 2488 ms (5.03%), Space: 14.3 MB (99.41%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -4,19 +4,12 @@ class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
         
         n = len(names)
-        
-        name_height_map = {}
-        for index, value in enumerate(names):
-            name_height_map[heights[index]] = value
-        
+    
         # sort using bubble sort
         for iteration in range(n):
             for index in range(1, n):
                 if heights[index] > heights[index - 1]:
                     heights[index], heights[index - 1] = heights[index - 1], heights[index]
+                    names[index], names[index - 1] = names[index - 1], names[index]
                 
-        output = []
-        for height in heights:
-            output.append(name_height_map[height])
-        
-        return output
+        return names
