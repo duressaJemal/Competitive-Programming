63d0ef70a6334c93d2e1ba9b330d0bafeaa689db
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2023-01-18 20:33:00 +0300

    Time: 208 ms (84.19%), Space: 15.3 MB (97.85%) - LeetHub

--- 0941-valid-mountain-array/0941-valid-mountain-array.py
+++ 0941-valid-mountain-array/0941-valid-mountain-array.py
@@ -1,27 +1,61 @@
+# optimized
+
+# Time: O(N)
+# Space: O(1)
+
 class Solution:
     def validMountainArray(self, arr: List[int]) -> bool:
         
         n = len(arr)
-        if n < 3:
-            return False
         
-        difference = []
+        increasing_index = 0
+        decreasing_index = n - 1
         
         for index in range(1, n):
-            difference.append(arr[index] - arr[index - 1])
-        
-        if difference[0] <= 0:
-            return False
-        
-        count = 0
-        for index in range(1, len(difference)):
-            if difference[index]:
-                if difference[index] * difference[index - 1] < 0:
-                    count += 1
+            if arr[index] - arr[index - 1] > 0:
+                increasing_index += 1
             else:
-                return False
+                break
+        
+        for index in range(n - 1, 0, - 1):
+            if arr[index - 1] > arr[index]:
+                decreasing_index -= 1
+            else:
+                break
+        
+        return 0 < increasing_index == decreasing_index < n - 1
+        
+            
+# Time: O(N)
+# Space: O(N)
 
-        return count == 1
+# class Solution:
+#     def validMountainArray(self, arr: List[int]) -> bool:
+        
+#         n = len(arr)
+#         if n < 3:
+#             return False
+        
+#         difference = []
+        
+#         for index in range(1, n):
+#             difference.append(arr[index] - arr[index - 1])
+        
+#         if difference[0] <= 0:
+#             return False
+        
+#         count = 0
+#         for index in range(1, len(difference)):
+#             if difference[index]:
+#                 if difference[index] * difference[index - 1] < 0:
+#                     count += 1
+#             else:
+#                 return False
+
+#         return count == 1
+    
+    
+    
     
         
         
