72c6b4f17fb8eb1c899ead290bda934eeb479cf8
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2023-01-18 20:33:31 +0300

    Time: 230 ms (49.69%), Space: 15.3 MB (97.85%) - LeetHub

--- 0941-valid-mountain-array/0941-valid-mountain-array.py
+++ 0941-valid-mountain-array/0941-valid-mountain-array.py
@@ -29,30 +29,30 @@ class Solution:
 # Time: O(N)
 # Space: O(N)
 
-# class Solution:
-#     def validMountainArray(self, arr: List[int]) -> bool:
+class Solution:
+    def validMountainArray(self, arr: List[int]) -> bool:
         
-#         n = len(arr)
-#         if n < 3:
-#             return False
+        n = len(arr)
+        if n < 3:
+            return False
         
-#         difference = []
+        difference = []
         
-#         for index in range(1, n):
-#             difference.append(arr[index] - arr[index - 1])
+        for index in range(1, n):
+            difference.append(arr[index] - arr[index - 1])
         
-#         if difference[0] <= 0:
-#             return False
+        if difference[0] <= 0:
+            return False
         
-#         count = 0
-#         for index in range(1, len(difference)):
-#             if difference[index]:
-#                 if difference[index] * difference[index - 1] < 0:
-#                     count += 1
-#             else:
-#                 return False
+        count = 0
+        for index in range(1, len(difference)):
+            if difference[index]:
+                if difference[index] * difference[index - 1] < 0:
+                    count += 1
+            else:
+                return False
 
-#         return count == 1
+        return count == 1
     
     
     
