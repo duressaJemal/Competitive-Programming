14143426ca37a6e165a424758c8b39cdce7dafb6
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2023-01-19 22:22:53 +0300

    Time: 31 ms (91.60%), Space: 14 MB (12.07%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -1,20 +1,20 @@
 # Time: O(N^2) (Insertion sort)
 # Space: O(1)
 
-# class Solution:
-#     def sortColors(self, nums: List[int]) -> None:
-#         """
-#         Do not return anything, modify nums in-place instead.
-#         """
-#         n = len(nums)
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
         
-#         for index in range(1, n):
-#             if nums[index] < nums[index - 1]:
-#                 for i in range(index, 0, -1):
-#                     if nums[i] < nums[i - 1]:
-#                         nums[i], nums[i - 1] = nums[i - 1], nums[i]
-#                     else:
-#                         break
+        for index in range(1, n):
+            if nums[index] < nums[index - 1]:
+                for i in range(index, 0, -1):
+                    if nums[i] < nums[i - 1]:
+                        nums[i], nums[i - 1] = nums[i - 1], nums[i]
+                    else:
+                        break
         
                         
         
