9a4375398939be6439e3e3f4837cf8f3808c26c7
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2023-01-20 21:34:12 +0300

    Time: 48 ms (83.34%), Space: 13.8 MB (97.03%) - LeetHub

--- 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.py
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.py
@@ -19,19 +19,3 @@ class Solution:
             output += list(range(less_than_target, less_than_target + equal_to_target))
         
         return output
-
-
-# Time: O(Nlog(N))
-# Space: O(N)
-
-# class Solution:
-#     def targetIndices(self, nums: List[int], target: int) -> List[int]:
-        
-#         output = []
-#         nums.sort()
-        
-#         for index, value in enumerate(nums):
-#             if value == target:
-#                 output.append(index)
-        
-#         return output
