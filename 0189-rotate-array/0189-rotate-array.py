16bb6eb35915d030f24c487135140b31402bb527
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2023-01-25 18:27:48 +0300

    Time: 770 ms (23.62%), Space: 25.8 MB (6.96%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.py
@@ -0,0 +1,26 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        
+        n = len(nums)
+        
+        for index in range(n):
+            current_index = (index + k) % n
+            prev_value = nums[index]
+            
+            if isinstance(prev_value, tuple):
+                prev_value = prev_value[0]
+            
+            nums[current_index] = (nums[current_index], prev_value)
+        
+        for index, value in enumerate(nums):
+            nums[index] = value[1]
+        
+        
+        
+            
+        
+            
+            
