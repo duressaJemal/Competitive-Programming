7b47ba5fdf4f521915e63fb644ba728841d55caa
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2023-01-25 22:13:37 +0300

    S

---
+++ code force/C. Alternating Subsequence.py
@@ -0,0 +1,30 @@
+# Link: https://codeforces.com/contest/1343/problem/C
+#Q: C. Alternating Subsequence
+
+# Time: O(N)
+# Space: O(1)
+
+t = int(input())
+while t:
+
+    n = int(input())
+    arr = list(map(int, input().split()))
+    output = 0
+
+    current_index = 0
+
+    while current_index < n:
+        max_value = arr[current_index]
+        if arr[current_index] > 0:
+            while current_index < n and arr[current_index] > 0:
+                max_value = max(max_value, arr[current_index])
+                current_index += 1
+        else:
+            while current_index < n and arr[current_index] < 0:
+                max_value = max(max_value, arr[current_index])
+                current_index += 1
+        output += max_value
+    
+    print(output)
+
+    t -= 1
