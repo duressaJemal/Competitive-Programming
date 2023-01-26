c547c303c1dd6000d637b6947677ebf8970f25c0
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2023-01-26 17:56:48 +0300

    Time: 84 ms (21.12%), Space: 13.9 MB (61.16%) - LeetHub

---
+++ 0763-partition-labels/0763-partition-labels.py
@@ -0,0 +1,26 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        
+        """
+        count total appearance of char
+        for the given value 
+        
+        """
+        n = len(s)
+        output = []
+        
+        last_index = defaultdict(int)
+        for index, value in enumerate(s):
+            last_index[value] = index
+        
+        max_index = 0
+        left = 0
+        
+        for index in range(n):
+            max_index = max(max_index, last_index[s[index]])
+            if index == max_index:
+                output.append(index - left + 1)
+                left = index + 1
+            
+        return output
+                
