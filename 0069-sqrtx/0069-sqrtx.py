99a20bdc80c5429ba41a0b9a0f73dd164aa7fedf
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2023-02-02 09:44:57 +0300

    Time: 38 ms (77.94%), Space: 13.9 MB (50.01%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -1,4 +1,19 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
         
-        return int(x ** 0.5)
+        l = 1 # the min value it could be
+        r = 2 ** 16
+        print(r)
+             
+        while r > l + 1:
+            mid = (l + r) // 2
+            squared = mid * mid
+            
+            if squared < x:
+                l = mid
+            elif squared > x:
+                r = mid
+            else:
+                return mid
+        
+        return l if x else 0
