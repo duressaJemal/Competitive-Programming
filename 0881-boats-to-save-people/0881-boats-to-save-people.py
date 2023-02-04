35d40c6ebb8ff72fe78d03d93a7ff4774255f589
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2023-02-04 16:24:54 +0300

    Time: 493 ms (44.96%), Space: 20.9 MB (65.40%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.py
+++ 0881-boats-to-save-people/0881-boats-to-save-people.py
@@ -1,3 +1,7 @@
+# Time: O(Nlog(N))
+# Space: O(1)
+
+
 class Solution:
     def numRescueBoats(self, people: List[int], limit: int) -> int:
         
@@ -12,10 +16,7 @@ class Solution:
         while left <= right:
             if people[left] + people[right] <= limit:
                 left += 1
-                right -= 1
-            else:
-                right -= 1
-            
+            right -= 1
             count += 1
         
         return count
