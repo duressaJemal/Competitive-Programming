faf80cbf60c222f3326f2eb3e4945f18e7a9744b
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2023-02-20 17:19:08 +0300

    Time: 66 ms (26.29%), Space: 17.9 MB (8.67%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -4,18 +4,37 @@
 #         self.val = x
 #         self.next = None
 
+
+# Time: O(N)
+# Space: O(1)
+
+# class Solution:
+#     def hasCycle(self, head: Optional[ListNode]) -> bool:
+        
+#         # phase 1
+#         fast = head
+#         slow = head
+        
+#         while fast and fast.next:
+#             fast = fast.next.next
+#             slow = slow.next
+            
+#             if slow == fast:
+#                 return True
+        
+#         return False
+
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
         
-        # phase 1
-        fast = head
-        slow = head
+        visited = set()
         
-        while fast and fast.next:
-            fast = fast.next.next
-            slow = slow.next
-            
-            if slow == fast:
+        while head:
+            if head in visited:
                 return True
+            
+            visited.add(head)
+            head = head.next
         
         return False
+        
