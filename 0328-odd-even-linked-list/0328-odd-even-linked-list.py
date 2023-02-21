230abd092c05853e19eb3b26490754a5a35f4a6e
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2023-02-21 16:22:26 +0300

    Time: 57 ms (17.66%), Space: 16.6 MB (68.00%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.py
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.py
@@ -4,52 +4,72 @@
 #         self.val = val
 #         self.next = next
 
+# From discuss section
+
+# Time: O(N)
+# space: O(1)
+
 class Solution:
     def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         
-        """
-        maintain even and odd heads
-        if current node is odd index:
-            link to odd nodex
-        else:
-            link to even nodes
+        if not head:
+            return head
         
-        link tail of odd nodes to head of even nodes
+        odd = head
+        even = head.next
+        even_head = even
         
-        
-        """
-        
-        odd_head = ListNode()
-        even_head = ListNode()
-        
-        odd_current = odd_head
-        even_current = even_head
-        
-        count = 1
-        current = head
-        
-        while current:
-            if count % 2 == 0:
-                even_current.next = current
-                current = current.next
-                even_current = even_current.next
-                even_current.next = None
-            else:
-                odd_current.next = current
-                current = current.next
-                odd_current = odd_current.next
-                odd_current.next = None
+        while even and even.next:
+            odd.next = odd.next.next # even.next(after update)
+            even.next = even.next.next # odd.next(after update)
             
-            count += 1
+            odd = odd.next
+            even = even.next
         
-        if odd_head.next:
-            head1 = odd_head.next
-            head2 = even_head.next
-            odd_current.next = head2
-            return head1
-        else:
-            return odd_head.next
+        odd.next = even_head
+        return head
+
+
+
+# # Time: O(N)
+# # Space: O(1)
+
+# class Solution:
+#     def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         
+#         odd_head = ListNode()
+#         even_head = ListNode()
+        
+#         odd_current = odd_head
+#         even_current = even_head
+        
+#         count = 1
+#         current = head
+        
+#         while current:
+#             if count % 2 == 0:
+#                 even_current.next = current
+#                 current = current.next
+#                 even_current = even_current.next
+#                 even_current.next = None
+#             else:
+#                 odd_current.next = current
+#                 current = current.next
+#                 odd_current = odd_current.next
+#                 odd_current.next = None
+            
+#             count += 1
+        
+#         if odd_head.next:
+#             head1 = odd_head.next
+#             head2 = even_head.next
+#             odd_current.next = head2
+#             return head1
+#         else:
+#             return odd_head.next
+        
+
+
         
         
         
