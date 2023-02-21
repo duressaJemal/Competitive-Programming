add81bfd581bfbfaa65d8a3f3a6ec26668e433f9
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2023-02-21 16:23:36 +0300

    Time: 51 ms (35.96%), Space: 16.7 MB (22.97%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.py
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.py
@@ -19,6 +19,16 @@ class Solution:
         even = head.next
         even_head = even
         
+        """
+        intuition for the while loop
+        
+        if length is even:
+            then even.next == None
+        if length is odd:
+            then even == None
+            
+        """
+        
         while even and even.next:
             odd.next = odd.next.next # even.next(after update)
             even.next = even.next.next # odd.next(after update)
@@ -31,42 +41,42 @@ class Solution:
 
 
 
-# # Time: O(N)
-# # Space: O(1)
+# Time: O(N)
+# Space: O(1)
 
-# class Solution:
-#     def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+class Solution:
+    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         
-#         odd_head = ListNode()
-#         even_head = ListNode()
+        odd_head = ListNode()
+        even_head = ListNode()
         
-#         odd_current = odd_head
-#         even_current = even_head
+        odd_current = odd_head
+        even_current = even_head
         
-#         count = 1
-#         current = head
+        count = 1
+        current = head
         
-#         while current:
-#             if count % 2 == 0:
-#                 even_current.next = current
-#                 current = current.next
-#                 even_current = even_current.next
-#                 even_current.next = None
-#             else:
-#                 odd_current.next = current
-#                 current = current.next
-#                 odd_current = odd_current.next
-#                 odd_current.next = None
+        while current:
+            if count % 2 == 0:
+                even_current.next = current
+                current = current.next
+                even_current = even_current.next
+                even_current.next = None
+            else:
+                odd_current.next = current
+                current = current.next
+                odd_current = odd_current.next
+                odd_current.next = None
             
-#             count += 1
+            count += 1
         
-#         if odd_head.next:
-#             head1 = odd_head.next
-#             head2 = even_head.next
-#             odd_current.next = head2
-#             return head1
-#         else:
-#             return odd_head.next
+        if odd_head.next:
+            head1 = odd_head.next
+            head2 = even_head.next
+            odd_current.next = head2
+            return head1
+        else:
+            return odd_head.next
         
 
 
