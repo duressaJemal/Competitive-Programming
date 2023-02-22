dbfb05d5b0a626a1cd12613d9fdfcfb543c9cecd
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2023-02-22 11:36:54 +0300

    Time: 128 ms (40.88%), Space: 13.9 MB (93.08%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -0,0 +1,30 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        
+        k = len(s1)
+        n = len(s2)
+        
+        counter_s1 = Counter(s1)
+        counter_s2 = Counter()
+        
+        left = 0
+        
+        for right in range(n):
+            
+            # add
+            counter_s2[s2[right]] += 1
+            
+            # validate
+            if right - left + 1 == k:
+                
+                # check
+                if counter_s1 == counter_s2:
+                    return True
+                
+                # remove
+                counter_s2[s2[left]] -= 1
+                left += 1
+        
+        return False
+            
+        
