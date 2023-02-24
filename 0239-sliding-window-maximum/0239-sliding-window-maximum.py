f019eb209004b4322127a323ffd5ae33859a18be
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2023-02-24 16:03:56 +0300

    Time: 1967 ms (35.29%), Space: 30 MB (45.12%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -4,45 +4,49 @@
 # Time: O(N) -- N = max(len(nums) / k  + 2 * len(nums))
 # space: O(k)
 
-# class Solution:
-#     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
         
-#         n, l, res, output = len(nums), 0, 0, []
-#         s1, s2, = [], []
-#         max_s1, max_s2 = [float("-inf")], [float("-inf")]
+        n, l, res, output = len(nums), 0, 0, []
+        s1, s2, = [], []
+        max_s1, max_s2 = [float("-inf")], [float("-inf")]
         
-#         def add(x):
-#             s2.append(x)
-#             max_s2.append(max(max_s2[-1], s2[-1]))
+        def add(x):
+            s2.append(x)
+            max_s2.append(max(max_s2[-1], s2[-1]))
 
             
-#         def remove():
-#             if not s1:
-#                 while s2:
-#                     s1.append(s2.pop())
-#                     max_s2.pop()
-#                     max_s1.append(max(max_s1[-1], s1[-1]))
+        def remove():
+            if not s1:
+                while s2:
+                    s1.append(s2.pop())
+                    max_s2.pop()
+                    max_s1.append(max(max_s1[-1], s1[-1]))
             
-#             s1.pop()
-#             max_s1.pop()
+            s1.pop()
+            max_s1.pop()
             
-#         def good(l, r):
-#             return r - l + 1 == k
+        def good(l, r):
+            return r - l + 1 == k
             
-#         r = 0
-#         while r < n:
-#             add(nums[r])
-#             while not good(l, r):
-#                 r += 1
-#                 add(nums[r])
+        r = 0
+        while r < n:
+            add(nums[r])
+            while not good(l, r):
+                r += 1
+                add(nums[r])
             
-#             res = max(max_s1[-1], max_s2[-1])
-#             output.append(res)
-#             remove()
-#             l += 1
-#             r += 1
+            res = max(max_s1[-1], max_s2[-1])
+            output.append(res)
+            remove()
+            l += 1
+            r += 1
         
-#         return output
+        return output
+
+    
+# Time: O(N)
+# Space: O(M) where, M = (N - k)
 
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
