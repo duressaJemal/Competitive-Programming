be956016505ea129d8ca78d9a05b13f4ccabbb56
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2023-03-02 10:52:13 +0300

    Time: 219 ms (70.50%), Space: 15.5 MB (81.51%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.py
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.py
@@ -8,22 +8,15 @@ class Solution:
         
         result = [-1] * (n)
         greater = []
-        hash_map = defaultdict(lambda : -1)
-        
         
         for index in range(2 * n):
             
-            current_index = index % n
-            value = nums[current_index]
+            curr = index % n
             
-            while greater and value > greater[-1][1]:
-                hash_map[greater.pop()[0]] = value
-            
-            greater.append((current_index, value))
-        
-        
-        for key in hash_map:
-            result[key] = hash_map[key]
+            while greater and nums[curr] > nums[greater[-1]]:
+                result[greater.pop()] = nums[curr]
+            # add
+            greater.append(curr)
         
         return result
             
