9f7f7b127d7ecda463b4ee2a299e030484a7ad5e
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2023-03-08 11:47:42 +0300

    Time: 50 ms (47.05%), Space: 15.3 MB (23.61%) - LeetHub

--- 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.py
+++ 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.py
@@ -5,6 +5,9 @@
 #         self.left = left
 #         self.right = right
 
+# Time: O(N)
+# Space: O(N)
+
 class Solution:
     def averageOfSubtree(self, root: Optional[TreeNode]) -> int:
         
