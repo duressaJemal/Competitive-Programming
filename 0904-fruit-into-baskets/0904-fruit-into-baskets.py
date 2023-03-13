589f9eee4391885db4457d9d8d0f48c400f637c8
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2023-03-13 22:42:58 +0300

    Time: 958 ms (56.95%), Space: 20.4 MB (29.01%) - LeetHub

---
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -0,0 +1,29 @@
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        
+        n = len(fruits)
+        
+        dic = defaultdict(int)
+        res = 0
+        
+        left = 0
+        
+        for right in range(n):
+            
+            # add
+            dic[fruits[right]] += 1
+            
+            while len(dic) > 2:
+                # remove
+                dic[fruits[left]] -= 1
+                if not dic[fruits[left]]:
+                    dic.pop(fruits[left])
+                
+                left += 1
+            
+            res = max(res, right - left + 1)
+        
+        return res
+            
+            
+            
