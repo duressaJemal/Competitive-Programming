c02470e1cceed42af64cad8654453602ac8259ad
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2023-03-20 21:06:03 +0300

    Time: 71 ms (5.42%), Space: 13.9 MB (63.39%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.py
@@ -0,0 +1,53 @@
+class Solution:
+    def compress(self, chars: List[str]) -> int:
+        
+        n = len(chars)
+        
+        left = 0
+        right = 0
+        
+        while right < n:
+            
+            cur = chars[left]
+            count = -1
+            
+            while right < n and chars[right] == cur:
+                right += 1
+                count += 1
+            
+            if count:
+                str_count = str(count + 1)
+                left += 1
+                
+                for char in str_count:
+                    chars[left] = char
+                    left += 1
+                
+                # remove unwanted
+                while left != right:
+                    chars[left] = "-1"
+                    left += 1
+            
+        
+            # adjust
+            left = right
+        
+        index = 0
+        
+        while index < len(chars):
+            
+            if chars[index] == "-1":
+                chars.pop(index)
+            else:
+                index += 1
+            
+        
+        
+        
+        
+        
+            
+            
+            
+            
+            
