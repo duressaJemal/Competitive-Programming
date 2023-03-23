c0ebd4f52a543805bb9f22a1cfba25d29035bca8
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2023-03-23 18:57:34 +0300

    Time: 152 ms (32.18%), Space: 15.2 MB (29.98%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.py
@@ -0,0 +1,31 @@
+# Link: https://leetcode.com/problems/missing-number/
+#Q: 268. Missing Number
+
+# Time: O(N)
+# Space: O(1)
+
+# class Solution:
+#     def missingNumber(self, nums: List[int]) -> int:
+        
+#         n = len(nums)
+#         return (n * (n + 1)) // 2 - sum(nums)
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        
+        nums.append("x")
+        n = len(nums)
+        
+        for cur in range(n):
+            while nums[cur] != "x" and nums[cur] != cur:
+                temp = nums[cur]
+                nums[cur] = nums[nums[cur]]
+                nums[temp] = temp
+        
+        for index in range(len(nums)):
+            if nums[index] == "x":
+                return index
+                
+        
+                
+                
