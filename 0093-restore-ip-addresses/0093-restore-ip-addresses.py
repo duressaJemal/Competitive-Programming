3d2491e80c7db91f9b16665bd6ffb3964868d343
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2023-03-24 06:55:33 +0300

    Time: 72 ms (14.19%), Space: 14 MB (27.29%) - LeetHub

---
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.py
@@ -0,0 +1,56 @@
+class Solution:
+    def restoreIpAddresses(self, s: str) -> List[str]:
+        
+        """
+        
+        # base case
+        if count == 4 and we used all the nums
+        
+        
+        - we need counting
+        - we need the know if it is invalid
+        
+        
+        """
+        
+        count = [0]
+        output = []
+        
+        def backtrack(arr, index):
+            
+            if index == len(s):
+
+                if len(arr) == 4:
+                    output.append(".".join(arr))
+                return
+            
+            for step in range(0, len(s) - index):
+                
+                cur = s[index : index + step + 1]
+                
+                if int(cur) > 255 or (cur[0] == "0" and len(cur) > 1):
+                    return
+    
+                    
+                # add
+                arr.append(cur)
+                backtrack(arr, index + step + 1)
+                # remove
+                arr.pop()
+
+            
+        backtrack([], 0)
+        return output
+            
+                
+
+                
+                
+                    
+            
+            
+            
+            
+            
+            
+                    
