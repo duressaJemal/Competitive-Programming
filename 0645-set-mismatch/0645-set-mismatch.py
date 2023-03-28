3a579ff72fc28c4720fb3ecb57250529b4f11910
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2023-03-28 11:18:33 +0300

    Time: 222 ms (36.22%), Space: 15.3 MB (92.50%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -0,0 +1,18 @@
+class Solution:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        
+        n = len(nums)
+        
+        for right in range(n):
+            
+            while nums[right] != right + 1:
+                
+                temp = nums[right]
+                if nums[right] != nums[temp - 1]:
+                    nums[right], nums[temp - 1] = nums[temp - 1], nums[right]
+                else:
+                    break
+        
+        for index, value in enumerate(nums):
+            if value != index + 1:
+                return [value, index + 1]
