c5ec3cb4c04cd4cfea68581255bd5d9cc72389b6
duressa <duressajemal3@gmail.com>
2023-04-01 12:47:49 +0300

    PS

--- code force/sieve_of_eratosthenes_optimized.py
+++ code force/sieve_of_eratosthenes_optimized.py
@@ -1,12 +1,12 @@
 
 
 
-def find_prime(num):
+# def find_prime(num):
 
-    is_prime = [True for _ in range(num + 1)]
-    is_prime[0] = False
-    is_prime[1] = False
+#     is_prime = [True for _ in range(num + 1)]
+#     is_prime[0] = False
+#     is_prime[1] = False
 
-    start = 2
-    end = math
-    for cur in range(start, )
+#     start = 2
+#     end = math
+#     for cur in range(start, )
