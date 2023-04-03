023b41e8913f1575a90c0102449bc307f9e6eb93
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2023-04-03 23:35:38 +0300

    Time: 6030 ms (27.57%), Space: 24.8 MB (60.49%) - LeetHub

---
+++ 2523-closest-prime-numbers-in-range/2523-closest-prime-numbers-in-range.py
@@ -0,0 +1,47 @@
+class Solution:
+    def closestPrimes(self, left: int, right: int) -> List[int]:
+        
+        def find_all_primes(end):
+            for index in range(2, end + 1):
+                cur = index
+                
+                if is_prime[cur]:
+                    while cur <= right:
+                        is_prime[cur] = False
+                        cur += index
+                    is_prime[index] = True
+                
+        mx = right
+        is_prime = [True for _ in range(mx + 1)]
+        is_prime[0] = False
+        is_prime[1] = False
+        
+        end = ceil(sqrt(mx)) # optimized seive
+        find_all_primes(end)
+        
+        res = []
+        for val in range(left, right + 1):
+            if is_prime[val]:
+                res.append(val)
+
+        # record the min prime gaps and primes
+        distance = [float("inf"), [0, 0]]
+        if len(res) > 1:
+            for index in range(1, len(res)):
+                distance = min(distance, [(res[index] - res[index - 1]), [res[index - 1], res[index]]]) 
+        else:
+            return [-1, -1]
+        
+        return [distance[1][0], distance[1][1]]                       
+        
+                               
+                               
+        
+        
+        
+        
+        
+        
+            
+            
+            
