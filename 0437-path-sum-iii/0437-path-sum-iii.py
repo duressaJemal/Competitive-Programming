1ab9ac9862e9d77c2b72cc5fa233f91f80ed81cc
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2023-04-04 23:57:09 +0300

    Time: 807 ms (21.22%), Space: 15.6 MB (13.91%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.py
+++ 0437-path-sum-iii/0437-path-sum-iii.py
@@ -5,39 +5,41 @@
 #         self.left = left
 #         self.right = right
 
-
-# Link: https://leetcode.com/problems/path-sum-iii/
-#Q: 437. Path Sum III
-
-# Time: O(N*2)
-# Space: O(N)
-
-
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
         
-        output = 0
-        def valid_path_sum(root, total):
-            if not root: return 0
-            total += root.val
-            if total == targetSum:
-                return 1 + valid_path_sum(root.left, total) + valid_path_sum(root.right, total)
-            else:
-                return valid_path_sum(root.left, total) + valid_path_sum(root.right, total)
-        
-        
-        def preorder_traversal(root):
-            nonlocal output
-            if not root: return
+        def dfs(root, prev):
             
-            output += valid_path_sum(root, 0)
-            preorder_traversal(root.left)
-            preorder_traversal(root.right)
+            if not root:
+                return
+            
+            cur = prev + root.val
+            if cur == targetSum:
+                count[0] += 1
+                
+            # includ
+            dfs(root.left, cur)
+            dfs(root.right, cur)
+            
+            # exclude
+            if root not in visited:
+                if root.val == targetSum:
+                    count[0] += 1
+                dfs(root.left, root.val)
+                dfs(root.right, root.val)
+                
+                visited.add(root)
+
+        count = [0]
+        visited = set()
         
-        preorder_traversal(root)
-        return output
+        if root:
+            if root.val == targetSum:
+                count[0] += 1
+
+            dfs(root.left, root.val)
+            dfs(root.right, root.val)
+        
+        return count[0]
             
             
-            
-        
-        
