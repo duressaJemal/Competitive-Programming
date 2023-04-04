27e31b3917da5a5075105b86e7cfb1af49abb8f5
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2023-04-05 00:34:48 +0300

    Time: 43 ms (22.94%), Space: 14 MB (6.03%) - LeetHub

---
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.py
@@ -0,0 +1,62 @@
+# Time: O(N)
+# Space: O(H)
+
+class Solution:
+    def bstFromPreorder(self, preorder: List[int]) -> Optional[TreeNode]:
+        
+        self.pre_index = 0
+        def dfs(low, high):
+            
+            if self.pre_index >= len(preorder) or low > high: return None
+            if low <= preorder[self.pre_index] <= high:
+                node = TreeNode(preorder[self.pre_index])
+                self.pre_index += 1
+            else:
+                return None
+        
+            node.left = dfs(low, node.val - 1)
+            node.right = dfs(node.val + 1, high)
+            
+            return node
+            
+        return dfs(float("-inf"), float("inf"))
+    
+    
+# Time: O(N)
+# Space: O(H)
+
+class Solution:
+    def bstFromPreorder(self, preorder: List[int]) -> Optional[TreeNode]:
+        
+        node = TreeNode(preorder[0])
+        stack = [node]
+        
+        for value in preorder[1:]:
+            
+            if stack[-1].val > value:
+                while stack[-1].val > value:
+                    stack[-1].left = TreeNode(value)
+                    stack.append(stack[-1].left)
+
+            else:
+                while stack and stack[-1].val < value:
+                    prev = stack.pop()
+                
+                prev.right = TreeNode(value)
+                stack.append(prev.right)
+                
+        return node
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
