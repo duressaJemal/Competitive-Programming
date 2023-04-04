4f6b9b8dbd10614c6cc2e254b26d6c404d71eafd
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2023-04-04 15:25:02 +0300

    Time: 32 ms (82.51%), Space: 13.8 MB (66.05%) - LeetHub

---
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.py
@@ -0,0 +1,32 @@
+class Solution:
+    def minSteps(self, n: int) -> int:
+        
+        len_left = n - 1
+        cur_length = 1
+        
+        operations = 0
+        copy_length = 1
+        
+        while len_left:
+            
+            # if the undivided segemnt can be divided equally with cur_length
+            if len_left % cur_length == 0: 
+
+                operations += 2
+                len_left -= cur_length
+                copy_length = cur_length
+                cur_length *= 2
+                
+            else:
+                
+                # for copying
+                if copy_length == cur_length:
+                    operations += 1
+                    
+                # for pasting 
+                operations += 1
+                len_left -= copy_length
+                cur_length += copy_length
+                
+        
+        return operations
