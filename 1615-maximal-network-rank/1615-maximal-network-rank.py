f1eccd5cc24e88aec75b04312eafbacf47c6d84a
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2023-04-06 21:51:20 +0300

    Time: 346 ms (60.32%), Space: 16.2 MB (23.48%) - LeetHub

--- 1615-maximal-network-rank/1615-maximal-network-rank.py
+++ 1615-maximal-network-rank/1615-maximal-network-rank.py
@@ -1,37 +1,26 @@
 class Solution:
     def maximalNetworkRank(self, n: int, roads: List[List[int]]) -> int:
-        
-        """
-        count the degree for each node
-        take the maximum ones
-        
-        from the maximums check if there exist a pair that is not connected.
-            if:
-                return max + max
-            else:
-                return max + max - 1
-        
-        """
-        
+
         counter = defaultdict(int)
         edge_list = set()
         
         for road in roads:
             counter[road[0]] += 1
             counter[road[1]] += 1
-            edge_list.add((road[0], road[1]))
+            edge_list.add(tuple(road))
         
-        pairs = []
-        for key in counter:
-            pairs.append(key)
+        pairs = list(counter.keys())
             
         mx = 0
         for i in range(len(pairs)):
             for j in range(i + 1, len(pairs)):
-                if (pairs[i], pairs[j]) not in edge_list and (pairs[j], pairs[i]) not in edge_list:
-                    mx = max(mx, counter[pairs[i]] + counter[pairs[j]])
+                start = pairs[i]
+                end = pairs[j]
+                
+                if (start, end) not in edge_list and (end, start) not in edge_list:
+                    mx = max(mx, counter[start] + counter[end])
                 else:
-                    mx = max(mx, (counter[pairs[i]] + counter[pairs[j]]) - 1)
+                    mx = max(mx, (counter[start] + counter[end]) - 1)
             
         return mx
         
