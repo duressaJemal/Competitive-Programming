e94fa69825f14884aec5661a8e46819ed7804285
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2023-04-10 17:57:49 +0300

    Time: 99 ms (78.41%), Space: 15.7 MB (34.27%) - LeetHub

---
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
@@ -0,0 +1,27 @@
+class Solution:
+    def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+        
+        def dfs(node, arr):
+            
+            if node == len(graph) - 1:
+                arr.append(node)
+                res.append(arr.copy())
+                arr.pop()
+                return
+            
+            visited.add(node)
+            arr.append(node)
+            
+            for child in graph[node]:
+                if child not in visited:
+                    dfs(child, arr)
+            
+            arr.pop()
+            visited.remove(node)
+            return
+        
+        visited = set()
+        res = []
+        dfs(0, [])
+        
+        return res
