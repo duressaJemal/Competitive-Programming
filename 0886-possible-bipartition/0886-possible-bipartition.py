ec9a7c064cc20174584ab1343e7f3901b6252610
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2023-04-10 12:50:23 +0300

    Time: 668 ms (98.28%), Space: 22.2 MB (15.17%) - LeetHub

---
+++ 0886-possible-bipartition/0886-possible-bipartition.py
@@ -0,0 +1,31 @@
+class Solution:
+    def possibleBipartition(self, n: int, dislikes: List[List[int]]) -> bool:
+        
+        # build graph
+        graph = defaultdict(list)
+        for u, v in dislikes:
+            graph[u].append(v)
+            graph[v].append(u)
+        
+        
+        def bipartite(node, color):
+            
+            visited[node] = color
+            cur = True
+            for child in graph[node]:
+                if child in visited:
+                    if visited[child] == color: return False
+                else:
+                    cur = cur and bipartite(child, 1 - color)
+                    if not cur:
+                        return False
+            return cur
+        
+        visited = {}
+        res = True
+        for node in graph:
+            if node not in visited:
+                res = res and bipartite(node, 1)
+        return res
+        
+                    
