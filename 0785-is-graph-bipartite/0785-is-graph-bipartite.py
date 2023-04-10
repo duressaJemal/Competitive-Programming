d5e37d64e031882a02683e7c278efd16922a54da
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2023-04-10 18:22:51 +0300

    Time: 180 ms (54.11%), Space: 14.6 MB (37.78%) - LeetHub

---
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isBipartite(self, graph: List[List[int]]) -> bool:
+        
+        def bipartite(node, color):
+            
+            visited[node] = color 
+            for child in graph[node]:
+                if child in visited:
+                    if visited[child] == color: return False
+                else:
+                    if not bipartite(child, 1 - color):
+                        return False     
+            return True
+        
+        n = len(graph)
+        visited = {}
+        
+        for node in range(n):
+            if node not in visited:
+                if not bipartite(node, 1):
+                    return False
+        
+        return True
+                
+                    
