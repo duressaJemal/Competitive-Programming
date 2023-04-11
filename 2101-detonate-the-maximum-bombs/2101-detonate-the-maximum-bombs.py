2d23f76c7df0997f7c0047d1c0b8ceb83af658ea
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2023-04-11 18:33:05 +0300

    Time: 1254 ms (28.45%), Space: 19.8 MB (5.09%) - LeetHub

---
+++ 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.py
@@ -0,0 +1,39 @@
+class Solution:
+    def maximumDetonation(self, bombs: List[List[int]]) -> int:
+        
+        # build graph
+        graph = defaultdict(set)
+        n = len(bombs)
+        
+        for i in range(n):
+            x1, y1, r1 = bombs[i]
+            for j in range(n):
+                if i != j:
+                    x2, y2, r2 = bombs[j]
+                    dis = sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
+                    if dis <= r1:
+                        graph[(x1, y1, i)].add((x2, y2, j))
+
+        def dfs(node):
+            
+            visited.add(node)
+            count = 1
+            for child in graph[node]:
+                if child not in visited:
+                    count += dfs(child)
+            return count
+        
+        res = 1
+        for node in list(graph.keys()):
+            visited = set()
+            res = max(res, dfs(node))
+        
+        return res
+            
+        
+        
+        
+        
+        
+                    
+                    
