483c8a56422ad229b8fb6f95309c8106404197ad
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2023-04-15 22:05:49 +0300

    Time: 60 ms (47.62%), Space: 14 MB (67.58%) - LeetHub

--- 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.py
+++ 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.py
@@ -1,7 +1,9 @@
+# Time: O(N)
+# Space: O(1)
+
 class Solution:
     def findGCD(self, nums: List[int]) -> int:
-
-        
+ 
         mx = 0
         mn = 1001
         
@@ -9,6 +11,7 @@ class Solution:
             mx = max(mx, val)
             mn = min(mn, val)
         
+        # gcd() returns greatest common divisor of two numbers
         return gcd(mx, mn)
-        
+
         
