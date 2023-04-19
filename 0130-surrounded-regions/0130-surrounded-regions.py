7041ae2b89d2a605fc824d63fe2b45efbfc9c83f
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2023-04-19 19:50:39 +0300

    Time: 151 ms (25.15%), Space: 16.1 MB (35.89%) - LeetHub

---
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -0,0 +1,48 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        
+        def dfs(row, col):
+            
+            # base cases
+            if row < 0 or row >= row_len or col < 0 or col >= col_len:
+                return
+            if board[row][col] != "O":
+                return
+            
+            # mark visited and change value
+            board[row][col] = "#"
+            
+            direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+            for x, y in direction:
+                nr = x + row
+                nc = y + col
+                
+                dfs(nr, nc)
+            
+            return
+            
+        
+        row_len = len(board)
+        col_len = len(board[0])
+
+        for row in range(row_len):
+            for col in range(col_len):
+                # only for outer "0"
+                if (row in [0, row_len - 1] or col in [0, col_len - 1]) and board[row][col] == "O":
+                    dfs(row, col)
+        
+        for row in range(row_len):
+            for col in range(col_len):
+                if board[row][col] == "O":
+                    board[row][col] = "X"
+        
+        for row in range(row_len):
+            for col in range(col_len):
+                if board[row][col] == "#":
+                    board[row][col] = "O"
+                    
+        
+                    
