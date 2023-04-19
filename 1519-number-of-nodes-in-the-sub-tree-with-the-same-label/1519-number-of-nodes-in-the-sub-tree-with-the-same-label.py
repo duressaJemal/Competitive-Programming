9fabf6a3908a09febc9b369c68faa6fa25f3ddc7
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2023-04-19 20:25:19 +0300

    Time: 2092 ms (75.80%), Space: 191.9 MB (41.99%) - LeetHub

---
+++ 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.py
@@ -0,0 +1,40 @@
+class Solution:
+    def countSubTrees(self, n: int, edges: List[List[int]], labels: str) -> List[int]:
+        
+        """
+        Count all labels till this node using postorder traversal
+        """
+        def postorder(node):
+            visited.add(node)
+            counter = [0] * 26
+            
+            for child in graph[node]:
+                if child not in visited:
+                    cur = postorder(child)
+                    for index in range(26):
+                        counter[index] += cur[index]
+                        
+            
+            # increment the total count of labels
+            current_label = labels[node]
+            current_index = ord(current_label) - ord("a")
+            counter[current_index] += 1
+            
+            # record
+            output[node] = counter[current_index]
+            
+            return counter
+    
+        # build the graph
+        graph = defaultdict(list)
+        for u, v in edges:
+            graph[u].append(v)
+            graph[v].append(u)
+        
+        output = [0] * n
+        visited = set()
+        
+        postorder(0)
+        
+        return output
+        
