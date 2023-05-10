e68bcb330ccbcfc69f3212097812ae13474b8dd2
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2023-05-10 18:46:20 +0300

    Time: 114 ms (22.08%), Space: 17.7 MB (21.31%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,31 +1,36 @@
-# Time: O(N)
-# Space: O(N)
-
 class Solution:
-    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+    def canFinish(self, numCourses: int, pre: List[List[int]]) -> bool:
         
-        # build graph
         graph = defaultdict(list)
-        for end, start in prerequisites:
+        in_degree = [0 for _ in range(numCourses)]
+        
+        for end, start in pre:
             graph[start].append(end)
-                
-        def cycle_detection(node):
-            color[node] = 1 # make it gray
-            for child in graph[node]:
-                if color[child] == 1:
-                    return False
-                if color[child] == -1: # if traversed before
-                    continue
-                else:
-                    if not cycle_detection(child):
-                        return False         
-            color[node] = -1 # make it black
-            return True
-         
-        color = [0] * numCourses
+            in_degree[end] += 1
+        
+        queue = deque([])
         for node in range(numCourses):
-            if not color[node]:
-                if not cycle_detection(node):
-                    return False
-        return True
-    
+            if not in_degree[node]:
+                queue.append(node)
+            
+        order = []
+        
+        while queue:
+            
+            l = len(queue)
+            for _ in range(l):
+                
+                node = queue.popleft()
+                order.append(node)
+                
+                for child in graph[node]:
+                    in_degree[child] -= 1
+                    if not in_degree[child]:
+                        queue.append(child)
+        
+        if len(order) == numCourses:
+            return True
+        else:
+            return False
+                
+            
