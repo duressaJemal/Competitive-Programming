ef2622231927dfeff3d2d4c48b21005c342bb52b
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2023-05-17 21:59:53 +0300

    Time: 783 ms (46.10%), Space: 20.1 MB (24.64%) - LeetHub

---
+++ 1462-course-schedule-iv/1462-course-schedule-iv.py
@@ -0,0 +1,64 @@
+class Solution:
+    def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
+        
+        
+        ancestor = [set() for _ in range(numCourses)]
+        graph = defaultdict(list)
+        in_degree = [0] * (numCourses + 1)
+        output = []
+        
+        for start, end in prerequisites:
+            graph[start].append(end)
+            in_degree[end] += 1
+            
+        queue = deque([])
+        for node in graph:
+            if not in_degree[node]:
+                queue.append(node)
+        
+        
+        while queue:
+            
+            l = len(queue)
+            for _ in range(l):
+                
+                node = queue.popleft()
+                for child in graph[node]:
+                    
+                    in_degree[child] -= 1
+                    if not in_degree[child]:
+                        queue.append(child)
+                        
+                    for anc in ancestor[node]:
+                        ancestor[child].add(anc)
+                    ancestor[child].add(node)
+                    
+
+        # process each query
+        for start, end in queries:
+            
+            if start in ancestor[end]:
+                output.append(True)
+            else:
+                output.append(False)
+
+        return output
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
