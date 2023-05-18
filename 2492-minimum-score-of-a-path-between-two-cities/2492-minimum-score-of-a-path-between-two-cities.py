4d62dbe4ca2e2c9922e243eeea341b348044670a
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2023-05-18 18:29:07 +0300

    Time: 1608 ms (79.03%), Space: 61 MB (81.46%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
@@ -1,3 +1,7 @@
+# Time: O(V + E)
+# Space: O(V)
+
+
 class Solution:
     def minScore(self, n: int, roads: List[List[int]]) -> int:
         
@@ -6,11 +10,9 @@ class Solution:
             p = node
             while node != parent[node]:
                 node = parent[node]
-            
             while p != node:
                 parent[p] = node
                 p = parent[p]
-            
             return node
             
             
@@ -19,29 +21,32 @@ class Solution:
             p_node1 = find(node1)
             p_node2 = find(node2)
             
+            if p_node1 == p_node2:
+                return
             
             if rank[p_node1] > rank[p_node2]:
                 parent[p_node2] = p_node1
-                score[p_node1] = min(score[p_node1], score[p_node2], distance)
+
             elif rank[p_node2] > rank[p_node1]:
                 parent[p_node1] = p_node2
-                score[p_node2] = min(score[p_node2], score[p_node1], distance)
         
             if rank[p_node1] == rank[p_node2]:
                 parent[p_node2] = p_node1
                 rank[p_node1] += 1
-                score[p_node1] = min(score[p_node1], score[p_node2], distance)
                 
         parent = [i for i in range(n + 1)]
         rank = [0 for i in range(n + 1)]
-        score = [float("inf") for i in range(n + 1)]
+        answer = float("inf")
         
         for node1, node2, distance in roads:
             union(node1, node2, distance)
         
-        root = find(1)
-        return score[root]
         
+        for node1, node2, distance in roads:
+            if find(1) == find(node1):
+                answer = min(answer, distance)
+                
+        return answer
         
 
         
