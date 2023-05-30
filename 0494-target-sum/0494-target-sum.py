12d3fd5d97da3cdbe86423f7fe822e845e6fa1ce
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2023-05-30 06:35:21 +0300

    Time: 282 ms (66.71%), Space: 44.4 MB (5.21%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -1,21 +1,16 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
         
+        @lru_cache(None)
         def dp(index, total):
-            
             # base case
             if index == n:
                 return 1 if total == target else 0
+
+            return dp(index + 1, total + nums[index]) + dp(index + 1, total - nums[index])
             
-            if (index, total) in memo:
-                return memo[(index, total)]
-            
-            memo[(index,total)] = dp(index + 1, total + nums[index]) + dp(index + 1, total - nums[index])
-            
-            return memo[(index, total)]
-        
         n = len(nums)
-        memo = defaultdict(int)
+
         return dp(0, 0)
             
             
