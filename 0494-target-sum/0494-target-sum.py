8287fed6be21c5470ce2600ffe241eb4594839b0
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2023-05-30 06:38:44 +0300

    Time: 261 ms (72.59%), Space: 44.4 MB (5.21%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -1,8 +1,12 @@
+# Time: O(N)
+# Space: O(N)
+
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
         
         @lru_cache(None)
         def dp(index, total):
+            
             # base case
             if index == n:
                 return 1 if total == target else 0
@@ -10,7 +14,6 @@ class Solution:
             return dp(index + 1, total + nums[index]) + dp(index + 1, total - nums[index])
             
         n = len(nums)
-
         return dp(0, 0)
             
             
