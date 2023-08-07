8c1629ee0ca2286d82dbe08675d91042d58ce9e3
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2023-08-07 20:17:12 +0300

    Time: 54 ms (76.18%), Space: 16.8 MB (64.57%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,67 +1,3 @@
-# Binary search
-
-# Time: O(log(M*N))
-# Space: O(1)
-
-class Solution:
-    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        
-        m = len(matrix)
-        n = len(matrix[0])
-        
-        left = 0
-        right = m * n
-        
-        while left + 1 < right:
-            
-            mid = (left + right) // 2
-            row = mid // n
-            col = mid % n
-            
-            if target == matrix[row][col]:
-                return True
-            elif target > matrix[row][col]:
-                left = mid
-            else:
-                right = mid
-            
-        return target == matrix[left // n][left % n]
-    
-# binary search
-
-# Time: O(M*log(N))
-# Space: O(1)
-
-class Solution:
-    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        
-        for row in matrix:
-            
-            is_found = False
-            
-            # do binary search
-            
-            left = -1
-            right = len(row) - 1
-            
-            while left + 1 < right:
-                
-                mid = (left + right) // 2
-                if target > row[mid]:
-                    left = mid
-                elif target < row[mid]:
-                    right = mid
-                else:
-                    is_found = True
-                    break
-            
-            if is_found or row[right] == target:
-                return True
-        
-        return False
-                
-# optimized
-
 # Time: O(M + N)
 # Space: O(1)
 
@@ -88,20 +24,5 @@ class Solution:
                     col += 1
                 else:
                     return False
-        
-        
-
-                
-                    
-                
-                
-                
-                
-        
-        
-        
-        
-        
-        
         
         
