e8c25e1e8bbd9d03eebc08aeade7fb552c9d07bc
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2023-09-01 22:49:54 +0300

    Time: 77 ms (70.10%), Space: 23 MB (65.12%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -21,6 +21,15 @@ class Solution:
     
     
     
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        
+        output = [0]
+        
+        for i in range(1, n + 1):
+            output.append(output[i // 2] + i % 2) 
+        
+        return output
         
             
             
