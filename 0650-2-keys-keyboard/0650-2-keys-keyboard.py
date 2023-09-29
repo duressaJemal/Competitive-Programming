31bfae3c61d973b91df351197d77977ee969a98e
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2023-09-29 16:56:10 +0300

    Time: 522 ms (13.70%), Space: 17.2 MB (38.10%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.py
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.py
@@ -1,17 +1,47 @@
+# class Solution:
+#     def minSteps(self, n: int) -> int:
+        
+#         @cache
+#         def dp(num):
+#             if num == 1:
+#                 return 0
+            
+#             mn = float("inf")
+            
+#             for i in range(1, num):
+#                 if num % i == 0:
+#                     mn = min(mn, dp(i) + (num // i))
+            
+#             return mn
+        
+#         return dp(n)
+    
+
 class Solution:
     def minSteps(self, n: int) -> int:
         
-        @cache
-        def dp(num):
-            if num == 1:
+        def dp(word_length, print_size):
+            
+            if word_length == n:
                 return 0
             
-            mn = float("inf")
+            if word_length > n:
+                return float("inf")
             
-            for i in range(1, num):
-                if num % i == 0:
-                    mn = min(mn, dp(i) + (num // i))
+            # copy and print into notepad
+            copy_and_print = 2 + dp(2 * word_length, word_length)
             
-            return mn
+            # print into notepad
+            if word_length > 1:
+                print_into_notepad = 1 + dp(word_length + print_size, print_size)
+            else:
+                print_into_notepad = copy_and_print
+            
+            return min(print_into_notepad, copy_and_print)
         
-        return dp(n)
+        return dp(1, 1)
+            
+            
+    
+
+
