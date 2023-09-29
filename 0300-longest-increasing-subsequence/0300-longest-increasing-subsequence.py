4b1f7c3d1cde008ae134bf4147b8941c231505bc
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2023-09-29 17:47:25 +0300

    Time: 2459 ms (35.75%), Space: 18.3 MB (6.54%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,17 +1,42 @@
+# class Solution:
+#     def lengthOfLIS(self, nums: List[int]) -> int:
+        
+#         n = len(nums)
+#         dp = [1] * n
+        
+#         for i in range(n):
+#             for j in range(0, i):
+                
+#                 if nums[j] < nums[i]:
+#                     dp[i] = max(dp[i], dp[j] + 1)
+            
+#         return max(dp)
+      
+
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         
+        @cache
+        def dp(index):
+            
+            if index == 0:
+                return 1
+
+            mx = 1
+            for i in range(index - 1, -1, -1):
+                if nums[i] < nums[index]:
+                    mx = max(mx, dp(i) + 1)
+                
+            return mx
+    
         n = len(nums)
         
-        dp = [1] * n
-        
+        output = 0
         for i in range(n):
-            for j in range(0, i):
-                
-                if nums[j] < nums[i]:
-                    dp[i] = max(dp[i], dp[j] + 1)
-            
+            output = max(output, dp(i))
         
-        return max(dp)
-            
+        return output
+        
+        
+                
             
