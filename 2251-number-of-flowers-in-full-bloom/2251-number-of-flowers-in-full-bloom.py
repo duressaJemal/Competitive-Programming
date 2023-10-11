075ed9ce2c8aeab5c2e6bd59889bff3c95fbb098
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2023-10-11 09:08:26 +0300

    Time: 915 ms (64.85%), Space: 44.6 MB (50.61%) - LeetHub

---
+++ 2251-number-of-flowers-in-full-bloom/2251-number-of-flowers-in-full-bloom.py
@@ -0,0 +1,22 @@
+class Solution:
+    def fullBloomFlowers(self, flowers: List[List[int]], people: List[int]) -> List[int]:
+        
+        n = len(flowers)
+        
+        s_first = sorted([a[0] for a in flowers])
+        s_second = sorted([a[1] for a in flowers])
+        
+        output = []
+        
+        for target in people:
+            
+            left_len = bisect.bisect_right(s_second, target - 1)
+            right_len = bisect.bisect_left(s_first, target + 1)
+            
+            output.append(n - (left_len + (n - right_len)))
+        
+        return output
+            
+            
+            
+            
