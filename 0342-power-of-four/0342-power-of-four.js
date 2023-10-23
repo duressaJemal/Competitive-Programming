ff043f77c5c48462dc8b3c2d91462e5fb9cb084a
duressaJemal <88533649+duressaJemal@users.noreply.github.com>
2023-10-23 17:26:04 +0300

    Time: 57 ms (69.34%), Space: 42.5 MB (91.16%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfFour = function(n) {
+    
+    if (n <= 0){
+        return false
+    }
+    
+  
+    while (n % 4 == 0){
+        n = Math.floor(n / 4);
+    }
+    
+    return n === 1;
+
+};
+
+
